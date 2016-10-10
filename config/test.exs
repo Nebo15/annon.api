6cffd6fc9939572d29570ee0ae443e788b471fe8
Eugene Pirogov <iamexile@gmail.com>
2016-10-10 17:58:32 +0300

    Make DB tests pass

--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,13 @@
 use Mix.Config
 
+config :gateway, Gateway.DB.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  priv: "priv/repos",
+  database: "gateway_test",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
 # Configuration for test environment
--- test/unit/http/api_test.exs
+++ test/unit/http/api_test.exs
@@ -3,6 +3,19 @@ defmodule Gateway.HTTP.APITest do
 
   use Plug.Test
 
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Gateway.DB.Repo)
+
+    # Ecto.Adapters.Postgres.storage_down(Gateway.DB.Repo.config)
+    # Ecto.Adapters.Postgres.storage_up(Gateway.DB.Repo.config)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(Gateway.DB.Repo, {:shared, self()})
+    end
+
+    :ok
+  end
+
   test "GET /apis" do
     data =
       [
@@ -15,7 +28,14 @@ defmodule Gateway.HTTP.APITest do
       conn(:get, "/")
       |> Gateway.HTTP.API.call([])
 
+    expected_resp = %{
+      meta: %{
+        code: 200,
+      },
+      data: data
+    }
+
     assert conn.status == 200
-    assert conn.resp_body == Poison.encode!(data)
+    assert conn.resp_body == Poison.encode!(expected_resp)
   end
 end
