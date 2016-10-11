4b5a72ff96c50a94bfb99bf6f6d272243863d85b
Eugene Pirogov <iamexile@gmail.com>
2016-10-11 13:45:17 +0300

    Add test config

--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,11 @@
 use Mix.Config
 
-# Configuration for test environment
+config :gateway, Gateway.DB.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  priv: "priv/repos",
+  database: "gateway_test",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
