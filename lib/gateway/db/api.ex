cacd151ff5e77cac0fa66afaedd6f2f4147429d3
Eugene Pirogov <iamexile@gmail.com>
2016-10-11 16:42:38 +0300

    OSL-380 (#12)
    * WIP
    
    * Stabilize the schema
    
    * Extract the json from body
    
    * WIP endpoint
    
    * Change DB schema for APIs
    
    * Make it possible to encode Gateway.DB.API
    
    Likely to be moved over to a dedicated helper module, shared by many
    others.
    
    * No need to generate a primary_key for embedded
    
    * Remove debug info
    
    * Create is working
    
    * Remove priv/migrations
    
    * Remove more debug info
    
    * Return validation errors
    
    * Implement GET
    
    * Fix variable name
    
    * Remove dead code
    
    * GET /apis
    
    * Very dirty, but working
    
    * Make DB tests pass
    
    * Remove comments
    
    * Remove comments
    
    * Move the contents to where it belongs
    
    * Add two more tests
    
    * Add comment
    
    * Clean up testing support code
    
    * Cleanup
    
    * Extract helper functions for responses
    
    * Extract common code for crud routes
    
    * Cleanup migrations
    
    * PUT /apis/:api_id
    
    * Add missing request headers
    
    * Remove debug info

---
+++ lib/gateway/db/api.ex
@@ -0,0 +1,61 @@
+defmodule Gateway.DB.API do
+  @moduledoc """
+  API DB entity
+  """
+
+  use Ecto.Schema
+
+  defimpl Poison.Encoder, for: Gateway.DB.API do
+    def encode(%{__struct__: _} = struct, options) do
+      map = struct
+            |> Map.from_struct
+            |> sanitize_map
+      Poison.Encoder.Map.encode(map, options)
+    end
+
+    defp sanitize_map(map) do
+      Map.drop(map, [:__meta__, :__struct__])
+    end
+  end
+
+  schema "apis" do
+    field :name, :string
+
+    embeds_one :request, Request, primary_key: false do
+      field :scheme, :string
+      field :host, :string
+      field :port, :string
+      field :path, :string
+    end
+
+    timestamps()
+  end
+
+  @required_api_fields [:name]
+  @required_request_fields [:scheme, :host, :port, :path]
+
+  def changeset(api, params \\ %{}) do
+    api
+    |> Ecto.Changeset.cast(params, @required_api_fields)
+    |> Ecto.Changeset.validate_required(@required_api_fields)
+    |> Ecto.Changeset.cast_embed(:request, with: &request_changeset/2)
+  end
+
+  def request_changeset(api, params \\ %{}) do
+    api
+    |> Ecto.Changeset.cast(params, @required_request_fields)
+    |> Ecto.Changeset.validate_required(@required_request_fields)
+  end
+
+  def create(params) do
+    api = %Gateway.DB.API{}
+    changeset = changeset(api, params)
+    Gateway.DB.Repo.insert(changeset)
+  end
+
+  def update(api_id, params) do
+    %Gateway.DB.API{ id: String.to_integer(api_id) }
+    |> changeset(params)
+    |> Gateway.DB.Repo.update()
+  end
+end
---
+++ priv/repos/migrations/20161007140320_add_apis_table.exs
@@ -0,0 +1,12 @@
+defmodule Gateway.DB.Repo.Migrations.AddApisTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:apis) do
+      add :name, :string
+      add :request, :map
+
+      timestamps()
+    end
+  end
+end
