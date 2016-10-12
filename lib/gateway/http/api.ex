a839270602489e2e9ecdaa8c02a497fe634d6a62
Eugene Pirogov <iamexile@gmail.com>
2016-10-12 13:32:57 +0300

    Import refactored response model
    Refactoring credits goes to @paulbardack

--- lib/gateway/http/api.ex
+++ lib/gateway/http/api.ex
@@ -1,62 +1,46 @@
 defmodule Gateway.HTTP.API do
   @moduledoc """
-  REST for api
+  REST for Api
   Documentation http://docs.osapigateway.apiary.io/#reference/apis
   """
   use Gateway.Helpers.CommonRouter
 
   get "/" do
-    apis = Gateway.DB.Repo.all(Gateway.DB.API)
-
-    { code, resp } =
-      render_show_response(apis)
-
-    send_resp(conn, code, resp)
+    Gateway.DB.API
+    |> Gateway.DB.Repo.all
+    |> render_show_response
+    |> send_response(conn)
   end
 
   get "/:api_id" do
-    { code, resp } =
-      case Gateway.DB.Repo.get(Gateway.DB.API, api_id) do
-        nil ->
-          render_not_found_response()
-        api ->
-          render_show_response(api)
-      end
-
-    send_resp(conn, code, resp)
+    Gateway.DB.API
+    |> Gateway.DB.Repo.get(api_id)
+    |> render_show_response
+    |> send_response(conn)
   end
 
   put "/:api_id" do
-    { code, resp } =
-      case Gateway.DB.API.update(api_id, conn.body_params) do
-        {:ok, api} ->
-          render_show_response(api)
-        {:error, changeset} ->
-          render_errors_response(changeset)
-      end
-
-    send_resp(conn, code, resp)
+    api_id
+    |> Gateway.DB.API.update(conn.body_params)
+    |> render_show_response
+    |> send_response(conn)
   end
 
   post "/" do
-    { code, resp } =
-      case Gateway.DB.API.create(conn.body_params) do
-        {:ok, api} ->
-          render_create_response(api)
-        {:error, changeset} ->
-          render_errors_response(changeset)
-      end
-
-    send_resp(conn, code, resp)
+    conn.body_params
+    |> Gateway.DB.API.create
+    |> render_create_response
+    |> send_response(conn)
   end
 
   delete "/:api_id" do
-    { code, resp } =
-      case Gateway.DB.API.delete(api_id) do
-        {:ok, api} ->
-          render_delete_response(api)
-      end
+    api_id
+    |> Gateway.DB.API.delete
+    |> render_delete_response
+    |> send_response(conn)
+  end
 
+  def send_response({code, resp}, conn) do
     send_resp(conn, code, resp)
   end
 end
