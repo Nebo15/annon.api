f00fec2b00a953bf10e25fcd994ad7a3efe1d7f3
Eugene Pirogov <iamexile@gmail.com>
2016-10-12 13:55:37 +0300

    Add consumers http endpoint + tests

---
+++ lib/gateway/http/consumers.ex
@@ -0,0 +1,46 @@
+defmodule Gateway.HTTP.Consumer do
+  @moduledoc """
+  REST for Consumers
+  Documentation http://docs.osapigateway.apiary.io/#reference/consumers
+  """
+  use Gateway.Helpers.CommonRouter
+
+  get "/" do
+    Gateway.DB.Consumer
+    |> Gateway.DB.Repo.all
+    |> render_show_response
+    |> send_response(conn)
+  end
+
+  get "/:consumer_id" do
+    Gateway.DB.Consumer
+    |> Gateway.DB.Repo.get(consumer_id)
+    |> render_show_response
+    |> send_response(conn)
+  end
+
+  put "/:consumer_id" do
+    consumer_id
+    |> Gateway.DB.Consumer.update(conn.body_params)
+    |> render_show_response
+    |> send_response(conn)
+  end
+
+  post "/" do
+    conn.body_params
+    |> Gateway.DB.Consumer.create
+    |> render_create_response
+    |> send_response(conn)
+  end
+
+  delete "/:consumer_id" do
+    consumer_id
+    |> Gateway.DB.Consumer.delete
+    |> render_delete_response
+    |> send_response(conn)
+  end
+
+  def send_response({code, resp}, conn) do
+    send_resp(conn, code, resp)
+  end
+end
---
+++ test/unit/http/consumer_test.ex
@@ -0,0 +1,27 @@
+defmodule Gateway.HTTP.APITest do
+  use Gateway.HTTPTestHelper
+
+  test "GET /consumers" do
+    data =
+      [
+        Gateway.DB.Consumer.create(%{ external_id: "SampleID1", metadata: %{}}),
+        Gateway.DB.Consumer.create(%{ external_id: "SampleID2", metadata: %{}})
+      ]
+      |> Enum.map(fn({:ok, e}) -> e end)
+
+    conn =
+      conn(:get, "/")
+      |> put_req_header("content-type", "application/json")
+      |> Gateway.HTTP.Consumer.call([])
+
+    expected_resp = %{
+      meta: %{
+        code: 200,
+      },
+      data: data
+    }
+
+    assert conn.status == 200
+    assert conn.resp_body == Poison.encode!(expected_resp)
+  end
+end
