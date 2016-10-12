5a7c84e4f8b36aed6354b048e2e54b8a8405b933
Eugene Pirogov <iamexile@gmail.com>
2016-10-12 17:25:16 +0300

    All tests are passing

--- test/unit/http/consumer_test.exs
+++ test/unit/http/consumer_test.exs
@@ -44,4 +44,57 @@ defmodule Gateway.HTTP.ConsumerTest do
     assert conn.status == 200
     assert conn.resp_body == Poison.encode!(expected_resp)
   end
+
+  test "POST /consumers" do
+    contents = %{
+      name: "Sample",
+      request: %{
+        host: "example.com",
+        port: "4000",
+        path: "/a/b/c",
+        scheme: "http"
+      }
+    }
+
+    conn =
+      conn(:post, "/", Poison.encode!(contents))
+      |> put_req_header("content-type", "application/json")
+      |> Gateway.HTTP.API.call([])
+
+    expected_resp = %{
+      meta: %{
+        code: 201,
+      },
+      data: contents
+    }
+
+    assert conn.status == 201
+    resp = Poison.decode!(conn.resp_body)["data"]
+
+    assert resp["id"]
+    assert resp["updated_at"]
+    assert resp["inserted_at"]
+
+    assert resp["name"] == "Sample"
+    assert resp["request"]["host"] == "example.com"
+    assert resp["request"]["port"] == "4000"
+    assert resp["request"]["path"] == "/a/b/c"
+    assert resp["request"]["scheme"] == "http"
+  end
+
+  test "DELETE /consumers/:api_id" do
+    { :ok, data } =
+      Gateway.DB.Consumer.create(%{ external_id: "SampleID1", metadata: %{}})
+
+    conn =
+      conn(:delete, "/#{data.id}")
+      |> put_req_header("content-type", "application/json")
+      |> Gateway.HTTP.Consumers.call([])
+
+    resp = Poison.decode!(conn.resp_body)
+
+    assert conn.status == 200
+    assert resp["data"]["id"] == data.id
+    assert resp["meta"]["description"] == "Resource was deleted"
+  end
 end
