b2a43bb05da78e7226b25c1b9f7e91d177c994db
Eugene Pirogov <iamexile@gmail.com>
2016-10-12 13:45:35 +0300

    Initial consumer model

---
+++ lib/gateway/db/customer.ex
@@ -0,0 +1,52 @@
+defmodule Gateway.DB.Consumer do
+  @moduledoc """
+  Consumer DB entity
+  """
+
+  use Ecto.Schema
+
+  defimpl Poison.Encoder, for: Gateway.DB.Consumer do
+    def encode(%{__struct__: _} = struct, options) do
+      map = struct
+            |> Map.from_struct
+            |> sanitize_map
+      Poison.Encoder.Map.encode(map, options)
+    end
+
+    defp sanitize_map(map) do
+      Map.drop(map, [:__meta__, :__struct__])
+    end
+  end
+
+  schema "consumers" do
+    field :external_id, :string
+    field :metadata, :map
+
+    timestamps()
+  end
+
+  @required_customer_fields [:name]
+
+  def changeset(api, params \\ %{}) do
+    api
+    |> Ecto.Changeset.cast(params, @required_consumer_fields)
+    |> Ecto.Changeset.validate_required(@required_consumer_fields)
+  end
+
+  def create(params) do
+    api = %Gateway.DB.Consumer{}
+    changeset = changeset(api, params)
+    Gateway.DB.Repo.insert(changeset)
+  end
+
+  def update(api_id, params) do
+    %Gateway.DB.Consumer{ id: String.to_integer(api_id) }
+    |> changeset(params)
+    |> Gateway.DB.Repo.update()
+  end
+
+  def delete(api_id) do
+    %Gateway.DB.Consumer{ id: String.to_integer(api_id) }
+    |> Gateway.DB.Repo.delete()
+  end
+end
