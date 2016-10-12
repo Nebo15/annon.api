788ddfc60de3aa8d95b4c0d0f6a5bee7cd53ecca
Eugene Pirogov <iamexile@gmail.com>
2016-10-12 14:01:25 +0300

    Rename the test

--- test/unit/http/consumer_test.exs
+++ test/unit/http/consumer_test.exs
@@ -1,4 +1,4 @@
-defmodule Gateway.HTTP.APITest do
+defmodule Gateway.HTTP.ConsumerTest do
   use Gateway.HTTPTestHelper
 
   test "GET /consumers" do
@@ -11,6 +11,26 @@ defmodule Gateway.HTTP.APITest do
 
     conn =
       conn(:get, "/")
+      |> put_req_header("content-type", "application/json")
+      |> Gateway.HTTP.Consumer.call([])
+
+    expected_resp = %{
+      meta: %{
+        code: 200,
+      },
+      data: data
+    }
+
+    assert conn.status == 200
+    assert conn.resp_body == Poison.encode!(expected_resp)
+  end
+
+  test "GET /consumers/:consumer_id" do
+    { :ok, data } =
+      Gateway.DB.Consumer.create(%{ external_id: "SampleID1", metadata: %{}})
+
+    conn =
+      conn(:get, "/#{data.id}")
       |> put_req_header("content-type", "application/json")
       |> Gateway.HTTP.Consumer.call([])
 
