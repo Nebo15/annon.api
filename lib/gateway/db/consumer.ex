99d626115748bba3db3e615c9500a35d43a06d45
Eugene Pirogov <iamexile@gmail.com>
2016-10-13 16:55:23 +0300

    OSL-381 (#22)
    * Initial consumer model
    
    * Fix attribute name
    
    * Fix file name
    
    * Add migration with consumers table
    
    * Add consumers http endpoint + tests
    
    * Fix required field attr
    
    * Correct migration
    
    * Rename the test
    
    * Fix copy/pasta
    
    * Correct module name (now: plural)
    
    * Fixes to tests
    
    * All tests are passing
    
    * Add broken PUT test
    
    * Fix final test
    
    * Actually :metadata field needs to be cast
    
    Otherwise it's lost
    
    * Finish test
    
    * Fix copy/pasta
    
    * id -> external_id
    
    This was easy.
    
    No more primary key ID. We fully rely on External ID now.
    
    * Fix last minute copy-pasta!
    
    * Cleanup poison encoding implementation
    
    * Remove :plugins key
    
    * Blame credo for this!
    
    * Blame dogma for this!
    
    * Enforce UUID
    
    * Satisfy dogma

---
+++ lib/gateway/db/consumer.ex
@@ -0,0 +1,41 @@
+defmodule Gateway.DB.Consumer do
+  @moduledoc """
+  Consumer DB entity
+  """
+
+  use Ecto.Schema
+
+  @derive {Poison.Encoder, except: [:__meta__]}
+
+  @primary_key {:external_id, Ecto.UUID, autogenerate: false}
+  schema "consumers" do
+    field :metadata, :map
+
+    timestamps()
+  end
+
+  @required_consumer_fields [:external_id]
+
+  def changeset(consumer, params \\ %{}) do
+    consumer
+    |> Ecto.Changeset.cast(params, @required_consumer_fields ++ [:metadata])
+    |> Ecto.Changeset.validate_required(@required_consumer_fields)
+  end
+
+  def create(params) do
+    consumer = %Gateway.DB.Consumer{}
+    changeset = changeset(consumer, params)
+    Gateway.DB.Repo.insert(changeset)
+  end
+
+  def update(consumer_id, params) do
+    %Gateway.DB.Consumer{external_id: consumer_id}
+    |> changeset(params)
+    |> Gateway.DB.Repo.update()
+  end
+
+  def delete(consumer_id) do
+    %Gateway.DB.Consumer{external_id: consumer_id}
+    |> Gateway.DB.Repo.delete()
+  end
+end
