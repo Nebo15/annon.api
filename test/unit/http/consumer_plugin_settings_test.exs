9634536c5e73c0d60c02009473fbc3c6071eade4
Eugene Pirogov <iamexile@gmail.com>
2016-10-17 19:39:35 +0300

    Test for GET method

--- test/unit/http/consumer_plugin_settings_test.exs
+++ test/unit/http/consumer_plugin_settings_test.exs
@@ -40,7 +40,34 @@ defmodule Gateway.HTTP.ConsumerPluginSettingsTest do
     assert Enum.count(x) == 2
   end
 
-  test "GET /consumers/:external_id/plugins/:name" do
+  test "GET /consumers/:external_id/plugins/:name", %{external_id: external_id, api: api} do
+    plugin_params1 =
+      Gateway.DB.Models.Plugin
+      |> EctoFixtures.ecto_fixtures()
+      |> Map.put(:api_id, api.id)
+
+    { :ok, plugin1 } = Gateway.DB.Models.Plugin.create(%Gateway.DB.Models.API{}, plugin_params1)
+
+    { :ok, cust_plugin1 } = Gateway.DB.Models.ConsumerPluginSettings.create(external_id, %{plugin_id: plugin1.id})
+
+    conn = :get
+    |> conn("/consumers/#{external_id}/plugins")
+    |> put_req_header("content-type", "application/json")
+    |> Gateway.HTTP.Consumers.call([])
+
+    [x] =
+      Poison.decode!(conn.resp_body)["data"]
+
+      # [%{"external_id" => "79f0c7ce-9487-11e6-bf61-002312123cdf", "id" => 10,
+      # [% "inserted_at" => "2016-10-17T16:33:46.945422", "plugin_id" => 81,
+      # [% "settings" => nil, "updated_at" => "2016-10-17T16:33:46.945438"}]
+
+    assert x["id"] == cust_plugin1.id
+    assert x["external_id"] == external_id
+    assert x["plugin_id"] == plugin1.id
+    assert x["settings"] == cust_plugin1.settings
+    assert x["inserted_at"]
+    assert x["updated_at"]
   end
 
   test "PUT /consumers/:external_id/plugins/:name" do
