258a4e084a501605fb3f39f0be0c6711c43fd064
vyarovoy <vladimir.yarovoy@finstar.com>
2016-10-17 11:28:47 +0300

    intermediary version

--- lib/gateway/monitoring.ex
+++ lib/gateway/monitoring.ex
@@ -1,11 +1,35 @@
 defmodule Gateway.Monitoring do
-    import Plug.Conn
-
-    def init(opts) do
+  import Plug.Conn
+  use Elixometer
+  
+  @unit :milli_seconds
+  
+  def init(opts) do
         opts
-    end
+  end
 
-    def call(conn, opts) do
-        IO.inspect Plug.Con..read_body(conn)
-    end    
+  def call(conn, opts) do
+    conn
+    |> metric_name("request_count")
+    |> update_counter(1)
+
+    req_start_time = :erlang.monotonic_time(@unit)
+    Plug.Conn.register_before_send conn, fn conn ->
+      request_duration = :erlang.monotonic_time(@unit) - req_start_time
+    
+    conn 
+    |> metric_name("request_duration")
+    |> update_histogram(request_duration)
+    
+    conn
+    end        
+  end
+
+  defp metric_name(conn, type) do
+    path = conn.path_info
+    |> Enum.reduce("", fn(x, acc) -> x <> "_" <> acc end)
+    
+    path <> "_" <> type
+  end
+
 end
--- lib/gateway/router.ex
+++ lib/gateway/router.ex
@@ -3,14 +3,11 @@ defmodule Gateway.Router do
   Gateway HTTP Router
   """
   use Plug.Router
-<<<<<<< 18a5156aad41506fe955663fd5698b0271a7b2d0
-=======
-  
+
   plug Gateway.Monitoring
   plug Plug.Parsers, parsers: [:json],
                      pass:  ["application/json"],
                      json_decoder: Poison
->>>>>>> initial commit
 
   plug :match
   plug :dispatch
