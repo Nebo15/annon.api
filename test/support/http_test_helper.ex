d69f7c72502348bfc0abd98e1c2b23c14a7ea956
samorai <oleg.samorai@gmail.com>
2016-10-17 13:39:14 +0300

    removing artifact

--- test/support/http_test_helper.ex
+++ test/support/http_test_helper.ex
@@ -7,10 +7,6 @@ defmodule Gateway.HTTPTestHelper do
       use ExUnit.Case, async: true
       use Plug.Test
 
-      def ins(a) do
-        IO.inspect(a)
-      end
-
       def assert_halt(%Plug.Conn{halted: true} = plug), do: plug
       def assert_not_halt(%Plug.Conn{halted: false} = plug), do: plug
 
