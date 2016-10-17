f043a4c1c880a9af5b8eb7b15ee56ccea8d4b758
samorai <oleg.samorai@gmail.com>
2016-10-17 07:53:15 +0300

    tech mac->imac

--- test/support/http_test_helper.ex
+++ test/support/http_test_helper.ex
@@ -7,6 +7,20 @@ defmodule Gateway.HTTPTestHelper do
       use ExUnit.Case, async: true
       use Plug.Test
 
+      def ins(a) do
+        IO.inspect(a)
+      end
+
+      def assert_halt(%Plug.Conn{halted: halted} = plug) do
+        assert halted === true
+        plug
+      end
+
+      def assert_not_halt(%Plug.Conn{halted: halted} = plug) do
+        assert halted === false
+        plug
+      end
+
       setup do
         :ok = Ecto.Adapters.SQL.Sandbox.checkout(Gateway.DB.Repo)
       end
--- test/unit/plugins/validator_test.exs
+++ test/unit/plugins/validator_test.exs
@@ -2,34 +2,39 @@ defmodule Gateway.Plugins.ValidatorTest do
   use Gateway.HTTPTestHelper
 
   test "user is redirected when current_user is not assigned" do
-    content = %{
-      "name" => "New name",
-      "surmame" => "Surname",
-      "age" => "100500",
-      "foo" =>  100500
-    }
-
     schema = %{
       "type" => "object",
       "properties" => %{
         "foo" => %{
           "type" => "number"
+        },
+        "bar" => %{
+          "type" => "string"
         }
-      }
+      },
+      "required" => ["bar"]
     }
 
-#    ExJsonSchema.Validator.valid?(schema, content)
-#    |> IO.inspect
+    connect = :get
+    |> conn("/", Poison.encode!(%{}))
 
-    :get
-    |> conn("/", Poison.encode!(content))
-    |> assign(:body, content)
+    connect
+    |> assign(:body, %{"foo" =>  "100500", "bar" => "a"})
     |> assign(:schema, schema)
     |> Gateway.Plugins.Validator.call(%{})
     |> assert_halt
 
+    connect
+    |> assign(:body, %{"foo" =>  100500, "bar" => "a"})
+    |> assign(:schema, schema)
+    |> Gateway.Plugins.Validator.call(%{})
+    |> assert_not_halt
 
-
+    connect
+    |> assign(:body, %{"foo" =>  100500})
+    |> assign(:schema, schema)
+    |> Gateway.Plugins.Validator.call(%{})
+    |> assert_halt
   end
 
 end
