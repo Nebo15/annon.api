5002c7a3ab6c30f6ec0aa6e3d1092e87e3b66a85
Eugene Pirogov <iamexile@gmail.com>
2016-10-17 13:58:00 +0300

    Make sure index is there

--- priv/repos/migrations/20161013140652_add_consumer_plugins.exs
+++ priv/repos/migrations/20161013140652_add_consumer_plugins.exs
@@ -3,11 +3,15 @@ defmodule Gateway.DB.Repo.Migrations.AddConsumerPluginSettings do
 
   def change do
     create table(:consumer_plugin_settings) do
-      add :consumer_id, :string
+      add :external_id, :string
       add :plugin_id, :integer
       add :settings, :map
 
       timestamps()
     end
+
+    index_name = :consumer_plugin_settings_external_id_plugin_id
+
+    create unique_index(:consumer_plugin_settings, [:external_id, :plugin_id], name: index_name)
   end
 end
