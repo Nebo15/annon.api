941b7d818f34d7812c6dc7abb8d85ba3c8363ddf
Oleg Samorai <oleg.samorai@gmail.com>
2016-10-18 11:15:36 +0300

    OSL-439 - Plugin Config Getter (Temporary Solution) (#34)
    * OSL-439 - Plugin Config Getter (Temporary Solution)
    
    * fix conflicts + merge latest master into OSL-439

--- test/support/http_test_helper.ex
+++ test/support/http_test_helper.ex
@@ -6,6 +6,22 @@ defmodule Gateway.HTTPTestHelper do
     quote do
       use ExUnit.Case, async: true
       use Plug.Test
+      alias Gateway.DB.Models.Plugin
+      alias Gateway.DB.Models.API, as: APIModel
+
+      def get_api_model_data do
+        api_model = APIModel
+        |> EctoFixtures.ecto_fixtures()
+
+        api_model
+        |> Map.put(:plugins, [get_plugin_data(api_model.id), get_plugin_data(api_model.id)])
+      end
+
+      def get_plugin_data(api_id) do
+        Plugin
+        |> EctoFixtures.ecto_fixtures()
+        |> Map.put(:api_id, api_id)
+      end
 
       setup do
         :ok = Ecto.Adapters.SQL.Sandbox.checkout(Gateway.DB.Repo)
