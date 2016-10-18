0e7322075142c177701712b0b6f249ef3dd6fa40
MBatrak <maxim.batrak@finstar.com>
2016-10-18 17:34:34 +0300

    Merge

--- lib/gateway/router.ex
+++ lib/gateway/router.ex
@@ -12,7 +12,6 @@ defmodule Gateway.Router do
   plug :dispatch
 
   forward "/apis", to: Gateway.HTTP.API
-<<<<<<< HEAD
 
   get "/" do
     send_resp(conn, 200, "{result: ok}")
@@ -21,7 +20,7 @@ defmodule Gateway.Router do
   match "/*_" do
     send_resp(conn, 200, "{result: default}")
   end
-=======
+
   forward "/consumers", to: Gateway.HTTP.Consumers
->>>>>>> baef22819ece86f3f6e6c09f68f0608a05d32ca7
+
 end
