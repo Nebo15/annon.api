e187ce1ffb0f362dcdb96ee76adaa6ac0c1a3d28
vyarovoy <vladimir.yarovoy@finstar.com>
2016-10-18 18:42:01 +0300

    [WIP] travis

--- bin/ci/init-statsd.sh
+++ bin/ci/init-statsd.sh
@@ -4,5 +4,5 @@ apt-get install -y nodejs
 apt-get install -y git
 git clone https://github.com/etsy/statsd.git
 cd statsd
-echo '{port: 8125, mgmt_port: 8126, backends: [\"./backends/console\"]}' > config.js
+echo '{port: 8125, mgmt_port: 8126, backends: ["./backends/console"]}' > config.js
 nodejs stats.js config.js
