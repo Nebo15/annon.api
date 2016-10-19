2b557ba342ba344a0212023e1327218945ca6ab8
Eugene Pirogov <iamexile@gmail.com>
2016-10-19 18:14:27 +0300

    Introduce PrivateRouter

--- lib/gateway.ex
+++ lib/gateway.ex
@@ -12,7 +12,8 @@ defmodule Gateway do
 
     children = [
       supervisor(Gateway.DB.Repo, []),
-      Plug.Adapters.Cowboy.child_spec(:http, Gateway.PublicRouter, [], public_http_config)
+      Plug.Adapters.Cowboy.child_spec(:http, Gateway.PublicRouter, [], public_http_config),
+      Plug.Adapters.Cowboy.child_spec(:http, Gateway.PrivateRouter, [], private_http_config)
     ]
 
     opts = [strategy: :one_for_one, name: Gateway.Supervisor]
--- lib/gateway/private_router.ex
+++ lib/gateway/private_router.ex
@@ -1,4 +1,4 @@
-defmodule Gateway.PublicRouter do
+defmodule Gateway.PrivateRouter do
   @moduledoc """
   Gateway HTTP Router
   """
@@ -8,9 +8,10 @@ defmodule Gateway.PublicRouter do
   plug Plug.Parsers, parsers: [:json],
                      pass: ["application/json"],
                      json_decoder: Poison
-  plug Gateway.Plugins.APILoader
-  plug Gateway.Plugins.Validator
   plug :dispatch
+
+  forward "/apis", to: Gateway.HTTP.API
+  forward "/consumers", to: Gateway.HTTP.Consumers
 
   match _ do
     send_resp(conn, 404, "{}")
