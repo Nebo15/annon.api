14832d84e7eaf4c09249de51efb39d1da4d8c71e
Eugene Pirogov <iamexile@gmail.com>
2016-10-19 12:48:43 +0300

    Remove unnecessary configs

--- config/test.exs
+++ config/test.exs
@@ -11,13 +11,6 @@ config :gateway, sql_sandbox: true
 
 memory_stats = ~w(atom binary ets processes total)a
 
-config(:exometer_core, report: [reporters: [{Gateway.Monitoring.TestReporter, []}]])
-
-config(:elixometer, update_frequency: 20,
-       reporter: Gateway.Monitoring.TestReporter,
-       env: Mix.env,
-       metric_prefix: "os.gateway")
-
 config :exometer,
    report: [
      reporters: [{Gateway.Monitoring.TestReporter, []}]
