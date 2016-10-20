b1002460c69e0ff99994d671dae2646801a66536
paul-nebo <paul@nebo15.com>
2016-10-20 17:06:52 +0300

    removed empty migration

--- priv/repos/migrations/20161020103044_consumer_plugins_set_external_id_as_uuid.exs
+++
@@ -1,9 +0,0 @@
-defmodule Gateway.DB.Repo.Migrations.ConsumerPluginsSetExternalIdAsUuid do
-  use Ecto.Migration
-
-  def change do
-#    alter table(:consumer_plugin_settings) do
-#      modify :external_id, :uuid
-#    end
-  end
-end
