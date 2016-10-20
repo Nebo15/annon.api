639a989850289137d0be4fb2b52c19baae01c372
Eugene Pirogov <iamexile@gmail.com>
2016-10-20 14:50:20 +0300

    Refactor tests: get rid of UUID

--- test/unit/http/consumer_test.exs
+++ test/unit/http/consumer_test.exs
@@ -4,8 +4,8 @@ defmodule Gateway.HTTP.ConsumerTest do
   test "GET /consumers" do
     data =
       [
-        Gateway.DB.Models.Consumer.create(%{ external_id: "123e4567-4321-12d3-a456-426655440000", metadata: %{}}),
-        Gateway.DB.Models.Consumer.create(%{ external_id: "321e4567-4321-12d3-a456-426655440000", metadata: %{}})
+        get_consumer_data |> Gateway.DB.Models.Consumer.create(),
+        get_consumer_data |> Gateway.DB.Models.Consumer.create()
       ]
       |> Enum.map(fn({:ok, e}) -> e end)
 
@@ -27,7 +27,8 @@ defmodule Gateway.HTTP.ConsumerTest do
 
   test "GET /consumers/:external_id" do
     { :ok, data } =
-      Gateway.DB.Models.Consumer.create(%{ external_id: "123e4567-4321-12d3-a456-426655440000", metadata: %{}})
+      get_consumer_data
+      |> Gateway.DB.Models.Consumer.create()
 
     conn = :get
       |> conn("/consumers/#{data.external_id}")
@@ -46,12 +47,7 @@ defmodule Gateway.HTTP.ConsumerTest do
   end
 
   test "POST /consumers" do
-    contents = %{
-      external_id: "321e4567-4321-12d3-a456-426655440000",
-      metadata: %{
-        existing_key: "some_value"
-      }
-    }
+    contents = get_consumer_data()
 
     conn = :post
       |> conn("/consumers", Poison.encode!(contents))
@@ -61,20 +57,19 @@ defmodule Gateway.HTTP.ConsumerTest do
     assert conn.status == 201
     resp = Poison.decode!(conn.resp_body)["data"]
 
-    assert resp["external_id"]
+    assert resp["external_id"] == contents[:external_id]
     assert resp["updated_at"]
     assert resp["inserted_at"]
-    assert resp["metadata"]["existing_key"] == "some_value"
+    assert resp["metadata"] == contents[:metadata]
   end
 
   test "PUT /consumers/:external_id" do
-    external_id = "123e4567-e89b-12d3-a456-426655440000"
-
     { :ok, data } =
-      Gateway.DB.Models.Consumer.create(%{ external_id: external_id, metadata: %{ existing_key: "some_value" }})
+      get_consumer_data
+      |> Gateway.DB.Models.Consumer.create()
 
     new_contents = %{
-      external_id: "123e4567-4321-12d3-a456-426655440000",
+      external_id: UUID.uuid1(),
       metadata: %{
         existing_key: "new_value",
         new_key: "another_value"
@@ -90,14 +85,15 @@ defmodule Gateway.HTTP.ConsumerTest do
     resp = Poison.decode!(conn.resp_body)["data"]
 
     assert resp["updated_at"]
-    assert resp["external_id"] == "123e4567-4321-12d3-a456-426655440000"
+    assert resp["external_id"] == new_contents[:external_id]
     assert resp["metadata"]["new_key"] == "another_value"
     assert resp["metadata"]["existing_key"] == "new_value"
   end
 
   test "DELETE /consumers/:external_id" do
     { :ok, data } =
-      Gateway.DB.Models.Consumer.create(%{ external_id: "321e4567-4321-12d3-a456-426655440000", metadata: %{}})
+      get_consumer_data
+      |> Gateway.DB.Models.Consumer.create()
 
     conn = :delete
       |> conn("/consumers/#{data.external_id}")
