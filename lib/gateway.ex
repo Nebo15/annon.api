d61409e56142e8e6de5f86283e6bfc12d0e0dd55
Eugene Pirogov <iamexile@gmail.com>
2016-10-20 11:56:14 +0300

    Fold child spec into a helper function

--- lib/gateway.ex
+++ lib/gateway.ex
@@ -7,16 +7,18 @@ defmodule Gateway do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    public_http_config = Confex.get_map(:gateway, :public_http)
-    private_http_config = Confex.get_map(:gateway, :private_http)
-
     children = [
       supervisor(Gateway.DB.Repo, []),
-      Plug.Adapters.Cowboy.child_spec(:http, Gateway.PublicRouter, [], public_http_config),
+      http_endpoint_spec(:private_http),
+      http_endpoint_spec(:public_http),
       Plug.Adapters.Cowboy.child_spec(:http, Gateway.PrivateRouter, [], private_http_config)
     ]
 
     opts = [strategy: :one_for_one, name: Gateway.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp endpoint_spec(type) do
+    Plug.Adapters.Cowboy.child_spec(:http, Gateway.PublicRouter, [], Confex.get_map(:gateway, type)),
   end
 end
