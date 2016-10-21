b0e60fc9ae6f36e4c314ea330c2e430c8648841d
Eugene Pirogov <iamexile@gmail.com>
2016-10-21 16:53:12 +0300

    Pipify the code

--- test/unit/monitoring/monitoring_test.exs
+++ test/unit/monitoring/monitoring_test.exs
@@ -23,8 +23,11 @@ defmodule Gateway.Monitoring.ElixometerTest do
   defp check_statsd(metric_type, metric_name) do
     {:ok, socket} = :gen_tcp.connect('localhost', 8126, [:list, {:active, false}])
     :ok = :gen_tcp.send(socket, metric_type)
-    {:ok, _ = response} = :gen_tcp.recv(socket, 0)
-    response = List.to_string(response)
-    String.contains?(response, metric_name)
+
+    socket
+    |> :gen_tcp.recv(0)
+    |> elem(1)
+    |> to_string
+    |> String.contains?(metric_name)
   end
 end
