a12ff37bd6f56c6c4f9a28857fc9e4180671bbda
Eugene Pirogov <iamexile@gmail.com>
2016-10-22 23:07:17 +0300

    Move monitoring under public router (#55)
    * Move monitoring under public router
    
    * Add TODO

--- lib/gateway/public_router.ex
+++ lib/gateway/public_router.ex
@@ -12,7 +12,14 @@ defmodule Gateway.PublicRouter do
   plug Gateway.Plugins.JWT
   plug Gateway.Plugins.ACL
   plug Gateway.Plugins.Validator
+  plug Gateway.Plugins.Monitoring
   plug :dispatch
+
+  # TODO: remove this route & update monitoring_test.exs
+  #       when @Samorai finishes OSL-383
+  get "/monitoring_test" do
+    send_resp(conn, 200, "Temporal route")
+  end
 
   match _ do
     send_resp(conn, 404, "{}")
