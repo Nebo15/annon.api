a46dd42fec9de157fb31744b353fb01072db88e0
Eugene Pirogov <iamexile@gmail.com>
2016-10-24 16:36:36 +0300

    Move method up

--- lib/gateway/config_guardian.ex
+++ lib/gateway/config_guardian.ex
@@ -5,6 +5,11 @@ defmodule Gateway.ConfigGuardian do
     GenServer.start_link(__MODULE__, [], [name: __MODULE__])
   end
 
+  def reload_config() do
+    send(__MODULE__, :reload_config)
+    Cluster.Events.publish(:reload_config)
+  end
+
   def init(_) do
     IO.inspect self()
     Cluster.Events.subscribe(self())
@@ -22,23 +27,12 @@ defmodule Gateway.ConfigGuardian do
     {:ok, []}
   end
 
-  def reload_config() do
-    send(__MODULE__, :reload_config)
-    Cluster.Events.publish(:reload_config)
-  end
-
   def handle_info(:reload_config, state) do
     IO.puts("#{inspect node()} is going to reload the config now!")
-    # query = from a in Gateway.DB.Models.API,
-    #         preload: [:plugins]
 
-    # models = query
-    # |> Gateway.DB.Repo.all()
-    {:noreply, state}
-  end
+    #   api = Gateway.DB.Models.API.get(1)
+    #   :ets.insert(:config, {{:apis, }}, )
 
-  def handle_info(any, state) do
-    IO.inspect any
     {:noreply, state}
   end
 end
