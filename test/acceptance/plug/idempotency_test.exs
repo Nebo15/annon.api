ff7f4bf1107f34615f80ce79d398352c2339bce0
paul-nebo <paul@nebo15.com>
2016-10-25 12:40:11 +0300

    idempotency test

--- test/acceptance/plug/idempotency_test.exs
+++ test/acceptance/plug/idempotency_test.exs
@@ -4,8 +4,13 @@ defmodule Gateway.Acceptance.Plug.IdempotencyTest do
   @idempotency_key UUID.uuid1()
 
   test "test idempotency DELETE request" do
-    "DELETE"
+
+    %HTTPoison.Response{body: body} = "GET"
     |> api_idempotency_data("/idempotency")
+    |> http_api_create()
+
+    "DELETE"
+    |> api_idempotency_data("/idempotency", Poison.decode!(body)["data"]["id"])
     |> http_api_create()
 
     "idempotency"
@@ -20,5 +25,28 @@ defmodule Gateway.Acceptance.Plug.IdempotencyTest do
     |> Map.put(:plugins, [
       %{name: "Idempotency", is_enabled: true, settings: %{"key" => 100}},
     ])
+  end
+
+  def api_idempotency_data(method, path, api_id) when is_integer(api_id) do
+    api_idempotency_data(method, path, Integer.to_string(api_id))
+  end
+  def api_idempotency_data(method, path, api_id) do
+    data = method
+    |> api_idempotency_data(path)
+
+    Map.put(data, :plugins, List.insert_at(
+      data.plugins,
+      -1,
+       %{ name: "Proxy", is_enabled: true,
+          settings: %{
+            "proxy_to" => Poison.encode!(%{
+              host: get_host(:private),
+              path: "/apis/#{api_id}",
+              port: get_port(:private),
+              scheme: "http"
+              })
+          }
+        }
+      ))
   end
 end
