16c43b35d296e26e7106578460b45a02f16e7f8c
Eugene Pirogov <iamexile@gmail.com>
2016-10-26 17:24:24 +0300

    Move test file

---
+++ test/unit/cluster_config_reloader_test.ex
@@ -0,0 +1,49 @@
+defmodule Gateway.ConfigReloaderTest do
+  use Gateway.UnitCase
+
+  @tag cluster: true
+  test "correct communication between processes" do
+    # spawns two nodes with "private" ports
+    # at 6001 and 6003 respectively
+    Gateway.Cluster.spawn()
+
+    {:ok, api} =
+      %{
+        name: "Test api",
+        request: %{
+          scheme: "HTTP",
+          host: "example.com",
+          port: "80",
+          path: "/",
+          method: "GET"
+        }
+      }
+      |> Gateway.DB.Models.API.create()
+
+    assert "Test api" == check_api_on_node(api.id, "name", 6001)
+    assert "Test api" == check_api_on_node(api.id, "name", 6003)
+
+    update_api(api.id, "name", "New name")
+
+    assert "New name" == check_api_on_node(api.id, "name", 6001)
+    assert "New name" == check_api_on_node(api.id, "name", 6003)
+
+    Gateway.DB.Models.API
+    |> Gateway.DB.Repo.delete_all()
+  end
+
+  defp check_api_on_node(api_id, field, port) do
+    "http://localhost:#{port}/apis/#{api_id}"
+    |> HTTPoison.get!()
+    |> Map.get(:body)
+    |> Poison.decode!
+    |> get_in(["data", field])
+  end
+
+  defp update_api(api_id, field, value) do
+    :put
+    |> conn("/apis/#{api_id}", Poison.encode!(%{field => value}))
+    |> put_req_header("content-type", "application/json")
+    |> Gateway.PrivateRouter.call([])
+  end
+end
