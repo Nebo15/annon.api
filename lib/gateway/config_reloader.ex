6b04279b17cdde38a720dfd158d0f5076c31442c
Eugene Pirogov <iamexile@gmail.com>
2016-10-27 15:01:25 +0300

    Remove todos

--- lib/gateway/config_reloader.ex
+++ lib/gateway/config_reloader.ex
@@ -8,14 +8,11 @@ defmodule Gateway.ConfigReloader do
 
   def call(conn, _) do
     destructive_method? = conn.method in ["POST", "PUT", "DELETE"]
-    successful_status? = true # conn.status in [200, 201]
+    successful_status? = conn.status in [200, 201]
 
     if destructive_method? && successful_status? do
       Gateway.ConfigGuardian.reload_config()
     end
-
-    # MAYBE also check if resp code is successfull
-    # MAYBE rename guardian and reloader
 
     conn
   end
