98dffcb3e9977d316c504ee99ea04ed6cef95826
Eugene Pirogov <iamexile@gmail.com>
2016-11-01 16:49:14 +0200

    Make the test more reliable

--- test/acceptance/cluster_config_reloader_test.exs
+++ test/acceptance/cluster_config_reloader_test.exs
@@ -13,30 +13,19 @@ defmodule Gateway.ClusterConfigReloaderTest do
     # node1@127.0.0.1 and node2@127.0.0.1
     Gateway.Cluster.spawn()
 
-    {:ok, api} =
-      %{
-        name: "Test api",
-        request: %{
-          scheme: "HTTP",
-          host: "example.com",
-          port: "80",
-          path: "/",
-          method: "GET"
-        }
-      }
-      |> Gateway.DB.Models.API.create()
+    api_id = create_api()
 
     Process.sleep(1000)
 
-    assert "Test api" == check_api_on_node(api.id, :name, :'node1@127.0.0.1')
-    assert "Test api" == check_api_on_node(api.id, :name, :'node2@127.0.0.1')
+    assert "Test api" == check_api_on_node(api_id, :name, :'node1@127.0.0.1')
+    assert "Test api" == check_api_on_node(api_id, :name, :'node2@127.0.0.1')
 
-    update_api(api.id, "name", "New name")
+    update_api(api_id, "name", "New name")
 
     Process.sleep(1000)
 
-    assert "New name" == check_api_on_node(api.id, :name, :'node1@127.0.0.1')
-    assert "New name" == check_api_on_node(api.id, :name, :'node2@127.0.0.1')
+    assert "New name" == check_api_on_node(api_id, :name, :'node1@127.0.0.1')
+    assert "New name" == check_api_on_node(api_id, :name, :'node2@127.0.0.1')
 
     Gateway.DB.Models.API
     |> Gateway.DB.Repo.delete_all()
@@ -55,5 +44,28 @@ defmodule Gateway.ClusterConfigReloaderTest do
     |> conn("/apis/#{api_id}", Poison.encode!(%{field => value}))
     |> put_req_header("content-type", "application/json")
     |> Gateway.PrivateRouter.call([])
+  end
+
+  defp create_api() do
+    map =
+      %{
+        name: "Test api",
+        request: %{
+          scheme: "HTTP",
+          host: "example.com",
+          port: "80",
+          path: "/",
+          method: "GET"
+        }
+      }
+
+    :post
+    |> conn("/apis", Poison.encode!(map))
+    |> put_req_header("content-type", "application/json")
+    |> Gateway.PrivateRouter.call([])
+    |> Map.get(:resp_body)
+    |> Poison.decode!
+    |> Map.get("data")
+    |> Map.get("id")
   end
 end
