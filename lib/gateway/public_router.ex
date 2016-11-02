1b7773540cab553d38f04e749e6868e5ed9d9ad1
Andrew Dryga <andrew@dryga.com>
2016-11-03 00:21:34 +0200

    Todos and reorder plugs

--- lib/gateway/public_router.ex
+++ lib/gateway/public_router.ex
@@ -10,16 +10,17 @@ defmodule Gateway.PublicRouter do
                      json_decoder: Poison
   plug Plug.RequestId
   plug Gateway.Plugins.APILoader
+  plug Gateway.Plugins.IPRestriction
+  plug Gateway.Plugins.Logger
+  plug Gateway.Plugins.Monitoring
   plug Gateway.Plugins.JWT
   plug Gateway.Plugins.ACL
   plug Gateway.Plugins.Idempotency
   plug Gateway.Plugins.Validator
-  plug Gateway.Plugins.Logger
-  plug Gateway.Plugins.Monitoring
-  plug Gateway.Plugins.IPRestriction
   plug Gateway.Plugins.Proxy
   plug :dispatch
 
+  # TODO: Use EView 404.json view
   match _ do
     send_resp(conn, 404, "{}")
   end
