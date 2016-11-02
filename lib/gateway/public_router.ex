6458716f99631a45507001c52b12930a880448a0
Eugene Pirogov <iamexile@gmail.com>
2016-11-02 19:33:40 +0200

    Actually remove TODO

--- lib/gateway/public_router.ex
+++ lib/gateway/public_router.ex
@@ -20,12 +20,6 @@ defmodule Gateway.PublicRouter do
   plug Gateway.Plugins.Proxy
   plug :dispatch
 
-  # TODO: remove this route & update monitoring_test.exs
-  #       when @Samorai finishes OSL-383
-  get "/monitoring_test" do
-    send_resp(conn, 200, "Temporal route")
-  end
-
   match _ do
     send_resp(conn, 404, "{}")
   end
