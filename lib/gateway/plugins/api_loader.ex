a0008815a761a4ad11376eeaaa8eede2d54b335b
Eugene Pirogov <iamexile@gmail.com>
2016-11-02 21:08:19 +0200

    Remove TODOs, debug statements and stale code

--- lib/gateway/plugins/api_loader.ex
+++ lib/gateway/plugins/api_loader.ex
@@ -11,7 +11,6 @@ defmodule Gateway.Plugins.APILoader do
 
   def call(conn, _), do: put_private(conn, :api_config, conn |> get_config)
 
-  # TODO: Get data from the cache, not from the DataBase
   def get_config(conn) do
     match_spec = %{
       request: %{
@@ -22,22 +21,11 @@ defmodule Gateway.Plugins.APILoader do
         path: conn.request_path
       }
     }
-    |> IO.inspect
 
-    :ets.tab2list(:config)
-    |> IO.inspect
-
-    IO.puts "In DB:"
-    Gateway.DB.Repo.all(Gateway.DB.Models.API)
-    |> IO.inspect
-
-
-    IO.puts "Result:"
     case :ets.match_object(:config, {:_, match_spec}) do
       [{_, api} | _] -> api
       _ -> nil
     end
-    |> IO.inspect
   end
 
   def normalize_scheme(scheme) when is_atom(scheme) do
@@ -45,6 +33,4 @@ defmodule Gateway.Plugins.APILoader do
   end
 
   def normalize_scheme(scheme), do: scheme
-
-  def equal_path?(%{path: path}, conn), do: conn.request_path == path
 end
