1e04df97c5cf8ebc42927730fdeca9120e4f92ec
Eugene Pirogov <iamexile@gmail.com>
2016-11-04 16:47:45 +0200

    Find matching APIs

--- lib/gateway/plugins/api_loader.ex
+++ lib/gateway/plugins/api_loader.ex
@@ -15,17 +15,25 @@ defmodule Gateway.Plugins.APILoader do
         host: conn.host,
         method: conn.method,
         port: conn.port,
-        scheme: normalize_scheme(conn.scheme),
-        path: conn.request_path
+        scheme: normalize_scheme(conn.scheme)
       }
     }
 
-    case :ets.match_object(:config, {:_, match_spec}) do
-      [{_, api} | _] -> api
-      _ -> nil
-    end
+    matching_apis =
+      :config
+      |> :ets.match_object({:_, match_spec})
+      |> find_matching_path(conn.request_path)
   end
 
   def normalize_scheme(scheme) when is_atom(scheme), do: Atom.to_string(scheme)
   def normalize_scheme(scheme), do: scheme
+
+  def find_matching_path(apis, path) do
+    apis
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.filter(&String.starts_with?(path, &1.request.path))
+    |> Enum.sort_by(&String.length(&1.request.path))
+    |> Enum.reverse
+    |> List.first
+  end
 end
