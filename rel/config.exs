5d656465d3d8c75bc320bc06cb36ea57ea3f6b2c
Andrew Dryga <andrew@dryga.com>
2016-11-05 00:15:01 +0200

    Get rid from dead rel code

--- rel/config.exs
+++ rel/config.exs
@@ -2,17 +2,11 @@ use Mix.Releases.Config,
   default_release: :default,
   default_environment: :default
 
-# TODO: Set from ENV vars in start-time via vm.args
-cookie = :sha256
-|> :crypto.hash(System.get_env("ERLANG_COOKIE") || "secret_erlang_cookie")
-|> Base.encode64
-
 environment :default do
   set pre_start_hook: "bin/hooks/pre-start.sh"
   set dev_mode: false
   set include_erts: false
   set include_src: false
-  set cookie: cookie
   set overlays: [
     {:template, "rel/templates/vm.args.eex", "releases/<%= release_version %>/vm.args"}
   ]
