e28a0dded49e16794e94dde3bb3693a0c5748948
Eugene Pirogov <iamexile@gmail.com>
2016-11-04 15:14:40 +0200

    Add request uniqness for APIs

---
+++ priv/repos/gateway/migrations/20161104121123_add_index_on_apis.exs
@@ -0,0 +1,13 @@
+defmodule Gateway.DB.Repo.Migrations.AddIndexOnApis do
+  use Ecto.Migration
+
+  def change do
+    columns = [
+      "(request->>'host')",
+      "(request->>'port')",
+      "(request->>'path')",
+      "(request->>'scheme')"
+    ]
+    create unique_index(:apis, columns, uniq: true, name: "api_unique_request_index")
+  end
+end
