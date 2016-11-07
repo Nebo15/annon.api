be31e799524c91054c1d5113a69a0d95e812421c
Eugene Pirogov <iamexile@gmail.com>
2016-11-07 17:51:31 +0200

    Simplify test code

--- lib/gateway/plugins/proxy.ex
+++ lib/gateway/plugins/proxy.ex
@@ -84,5 +84,13 @@ defmodule Gateway.Plugins.Proxy do
   defp put_port(pr, %{}), do: pr
 
   defp put_path(pr, %{"path" => path}, _conn), do: pr <> path
-  defp put_path(pr, %{}, %Plug.Conn{request_path: path}), do: pr <> path
+  defp put_path(pr, %{}, %Plug.Conn{request_path: path, private: private}) do
+    api_config = private.api_config
+
+    if api_config.strip_request_path do
+      pr <> String.trim_leading(path, api_config.request.path)
+    else
+      pr <> path
+    end
+  end
 end
--- test/unit/plugins/proxy_test.exs
+++ test/unit/plugins/proxy_test.exs
@@ -15,8 +15,6 @@ defmodule Gateway.Plugins.ProxyTest do
     assert Proxy.make_link(@proxy_settings_full, conn) == "http://localhost:4000/proxy/test"
     assert Proxy.make_link(@proxy_settings_port, conn) == "http://localhost/proxy/test"
     assert Proxy.make_link(@proxy_settings_scheme, conn) == "https://localhost/proxy/test"
-    assert Proxy.make_link(@proxy_settings_path, conn) == "https://localhost/proxy"
-    assert Proxy.make_link(@proxy_settings_just_host, conn) == "https://localhost/some/path"
 
     headers = [%{"random_name1" => "random_value1"}, %{"random_name2" => "random_value2"}]
     conn = Proxy.add_additional_headers(headers, conn)
@@ -26,5 +24,40 @@ defmodule Gateway.Plugins.ProxyTest do
     assert ["random_value1"] == random_value1
     assert ["random_value2"] == random_value2
     assert [ip_to_string(conn.remote_ip)] == x_forwarded_for_value
+  end
+
+  test "proxying path 1" do
+    conn = make_conn("/mockbin", false)
+    assert Proxy.make_link(@proxy_settings_just_host, conn) == "https://localhost/mockbin"
+  end
+
+  test "proxying path 2" do
+    conn = make_conn("/mockbin/some/path", false)
+    assert Proxy.make_link(@proxy_settings_just_host, conn) == "https://localhost/mockbin/some/path"
+  end
+
+  test "proxying path 3" do
+    conn = make_conn("/mockbin", true)
+    assert Proxy.make_link(@proxy_settings_just_host, conn) == "https://localhost"
+  end
+
+  test "proxying path 4" do
+    conn = make_conn("/mockbin/some/path", true)
+    assert Proxy.make_link(@proxy_settings_just_host, conn) == "https://localhost/some/path"
+  end
+
+  defp make_conn(request_path, strip_request_path) do
+    %Plug.Conn{
+      scheme: :https,
+      port: 6000,
+      request_path: request_path,
+      remote_ip: {127, 0, 0, 1},
+      private: %{
+        api_config: %{
+          strip_request_path: strip_request_path,
+          request: %{ path: "/mockbin" }
+        }
+      }
+    }
   end
 end
