a251626f21758f1a91700cbada302d1bf911fba0
Eugene Pirogov <iamexile@gmail.com>
2016-11-07 16:12:01 +0200

    Exclude pending always

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
-ExUnit.start()
+ExUnit.start(exclude: [:pending])
 Ecto.Adapters.SQL.Sandbox.mode(Gateway.DB.Configs.Repo, {:shared, self()})
 Ecto.Adapters.SQL.Sandbox.mode(Gateway.DB.Logger.Repo, {:shared, self()})
