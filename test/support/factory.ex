18174276b79a1347efcf3a34e4486b7c0b28ae1e
Eugene Pirogov <iamexile@gmail.com>
2016-11-08 16:24:42 +0200

    Add more factories

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,8 @@
 defmodule Gateway.Factory do
   use ExMachina.Ecto, repo: Gateway.DB.Configs.Repo
 
+  # APIs
+
   def api_factory do
     %Gateway.DB.Schemas.API{
       name: sequence(:api_name, &"An API ##{&1}"),
@@ -8,7 +10,18 @@ defmodule Gateway.Factory do
     }
   end
 
-  def request_factory() do
+  def api_with_default_plugins_factory do
+    %Gateway.DB.Schemas.API{
+      name: sequence(:api_name, &"An API ##{&1}"),
+      request: build(:request),
+      plugins: [
+        build(:jwt_plugin),
+        build(:acl_plugin)
+      ]
+    }
+  end
+
+  def request_factory do
     %Gateway.DB.Schemas.API.Request{
       method: "GET",
       scheme: "http",
@@ -18,12 +31,33 @@ defmodule Gateway.Factory do
     }
   end
 
+  # Plugin
+
   def plugin_factory do
     %Gateway.DB.Schemas.Plugin{
       name: "Jane Smith",
       is_enabled: true,
       settings: %{},
       api: build(:api)
+    }
+  end
+
+  def proxy_plugin_factory do
+  end
+
+  def jwt_plugin_factory do
+    %Gateway.DB.Schemas.Plugin{
+      name: "jwt",
+      is_enabled: true,
+      settings: %{"signature" => "secret-sign"}
+    }
+  end
+
+  def acl_plugin_factory do
+    %Gateway.DB.Schemas.Plugin{
+      name: "acl",
+      is_enabled: true,
+      settings: %{"scope" => "read"}
     }
   end
 end
