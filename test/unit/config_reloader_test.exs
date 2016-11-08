77df37cc709fcc49964326adc61f2edb533ead28
Eugene Pirogov <iamexile@gmail.com>
2016-11-08 16:41:08 +0200

    config_reloader_test.exs

--- test/unit/config_reloader_test.exs
+++ test/unit/config_reloader_test.exs
@@ -4,9 +4,7 @@ defmodule Gateway.ConfigReloaderTest do
   import ExUnit.CaptureLog
 
   test "reload the config cache if it changes" do
-    {:ok, api_model} =
-      get_api_model_data()
-      |> Gateway.DB.Schemas.API.create()
+    api_model = Gateway.Factory.insert(:api_with_default_plugins)
 
     new_contents = %{
       "name" => "New name"
