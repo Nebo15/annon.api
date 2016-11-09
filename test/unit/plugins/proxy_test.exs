bd9e14582d06fd74992452e094a72b75b984bbe2
Eugene Pirogov <iamexile@gmail.com>
2016-11-09 15:56:45 +0200

    Correctly test the case

--- test/unit/plugins/proxy_test.exs
+++ test/unit/plugins/proxy_test.exs
@@ -10,12 +10,12 @@ defmodule Gateway.Plugins.ProxyTest do
   @proxy_settings_just_host %{"host" => "localhost"}
 
   test "proxy" do
-    conn = %Plug.Conn{scheme: :https, port: 6000, request_path: "/some/path?key=value", remote_ip: {127, 0, 0, 1}}
+    conn = %Plug.Conn{scheme: :https, port: 6000, request_path: "/some/path", query_string: "key=value", remote_ip: {127, 0, 0, 1}}
 
-    assert Proxy.make_link(@proxy_settings_full, conn) == "http://localhost:4000/proxy/test"
-    assert Proxy.make_link(@proxy_settings_port, conn) == "http://localhost/proxy/test"
-    assert Proxy.make_link(@proxy_settings_scheme, conn) == "https://localhost/proxy/test"
-    assert Proxy.make_link(@proxy_settings_path, conn) == "https://localhost/proxy"
+    assert Proxy.make_link(@proxy_settings_full, conn) == "http://localhost:4000/proxy/test?key=value"
+    assert Proxy.make_link(@proxy_settings_port, conn) == "http://localhost/proxy/test?key=value"
+    assert Proxy.make_link(@proxy_settings_scheme, conn) == "https://localhost/proxy/test?key=value"
+    assert Proxy.make_link(@proxy_settings_path, conn) == "https://localhost/proxy?key=value"
     assert Proxy.make_link(@proxy_settings_just_host, conn) == "https://localhost/some/path?key=value"
 
     headers = [%{"random_name1" => "random_value1"}, %{"random_name2" => "random_value2"}]
