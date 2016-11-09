c72a88dd1676a80fd46950023c1852006cdda2b2
Eugene Pirogov <iamexile@gmail.com>
2016-11-09 16:52:04 +0200

    Finish tests for ACL

--- test/smoke_tests/acl_check_test.ex
+++ test/smoke_tests/acl_check_test.ex
@@ -33,16 +33,24 @@ defmodule Gateway.SmokeTests.BasicProxy do
       }
     })
 
+    Gateway.DB.Schemas.Plugin.create(api.id, %{
+      name: "jwt",
+      is_enabled: true,
+      settings: %{
+        "signature" => "a_secret_signature"
+      }
+    })
+
     Gateway.AutoClustering.do_reload_config()
 
     :ok
   end
 
-  test "A request from user is forbidden to access upstream" do
+  test "A request with no auth header is forbidden to access upstream" do
     api_endpoint = "#{get_host(:public)}:#{get_port(:public)}"
 
     response =
-      "http://#{api_endpoint}/httpbin"
+      "http://#{api_endpoint}/httpbin?my_param=my_value"
       |> HTTPoison.get!
       |> Map.get(:body)
       |> Poison.decode!
@@ -50,5 +58,35 @@ defmodule Gateway.SmokeTests.BasicProxy do
     assert "Your scopes does not allow to access this resource." == response["error"]["message"]
     assert "forbidden" == response["error"]["type"]
     assert 403 == response["meta"]["code"]
+  end
+
+  test "A request with incorrect auth header is forbidden to access upstream" do
+    api_endpoint = "#{get_host(:public)}:#{get_port(:public)}"
+
+    auth_token = jwt_token(%{"scopes" => ["httpbin:wrong"]}, "a_wrong_secret_signature")
+
+    response =
+      "http://#{api_endpoint}/httpbin?my_param=my_value"
+      |> HTTPoison.get!
+      |> Map.get(:body)
+      |> Poison.decode!
+
+    assert "There are no JWT token in request or your token is invalid." == response["error"]["message"]
+    assert "access_denied" == response["error"]["type"]
+    assert 401 == response["meta"]["code"]
+  end
+
+  test "A request with good auth header is allowed to access upstream" do
+    api_endpoint = "#{get_host(:public)}:#{get_port(:public)}"
+
+    auth_token = jwt_token(%{"scopes" => ["httpbin:read"]}, "a_secret_signature")
+
+    response =
+      "http://#{api_endpoint}/httpbin?my_param=my_value"
+      |> HTTPoison.get!([{"authorization", "Bearer #{auth_token}"}])
+      |> Map.get(:body)
+      |> Poison.decode!
+
+    assert "my_value" == response["args"]["my_param"]
   end
 end
