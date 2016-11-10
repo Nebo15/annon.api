1e2ee851f59043227935d1365c9f2b6d7977ecae
Andrew Dryga <andrew@dryga.com>
2016-11-10 20:20:23 +0200

    Drop cases and helpers that is not used

--- test/acceptance/plug/jwt_test.exs
+++ test/acceptance/plug/jwt_test.exs
@@ -1,7 +1,6 @@
 defmodule Gateway.Acceptance.Plug.JWTTest do
   @moduledoc false
   use Gateway.AcceptanceCase
-  alias Gateway.Test.Helper
 
   @api_url "apis"
   @consumer_url "consumers"
@@ -21,7 +20,7 @@ defmodule Gateway.Acceptance.Plug.JWTTest do
                      },
                      "required" => ["foo"]
                    }
-  @consumer_id Helper.random_string(32)
+  @consumer_id random_string(32)
   @consumer %{
     external_id: @consumer_id,
     metadata: %{"key": "value"},
