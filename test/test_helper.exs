7e443d1465c0df119497a00344f0e823ea9ea051
Andrew Dryga <andrew@dryga.com>
2016-11-10 23:40:38 +0200

    Introduce Fake Server for test purposes

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,16 @@
-ExUnit.start(exclude: [:pending])
+# Setup fake server
+FakeServer.Status.create(:status200, %{response_code: 200, response_body: "Hello World"})
+{:ok, address} = FakeServer.run(:external_server, [], %{port: 9090})
+
+# Write it's url to env
+Application.put_env(:gateway, :fake_server_url, address)
+
+# Start Factory service
+{:ok, _} = Application.ensure_all_started(:ex_machina)
+
+# Switch SQL sandbox to manual mode
 Ecto.Adapters.SQL.Sandbox.mode(Gateway.DB.Configs.Repo, :manual)
 Ecto.Adapters.SQL.Sandbox.mode(Gateway.DB.Logger.Repo, :manual)
-{:ok, _} = Application.ensure_all_started(:ex_machina)
+
+# Start tests
+ExUnit.start(exclude: [:pending])
