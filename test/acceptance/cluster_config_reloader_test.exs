551e8c0dfb53e5e9350f85c0769856aef95fa3b9
Andrew Dryga <andrew@dryga.com>
2016-11-11 16:10:46 +0200

    Failed to fix cluster test :D

--- test/acceptance/cluster_config_reloader_test.exs
+++ test/acceptance/cluster_config_reloader_test.exs
@@ -1,20 +1,27 @@
 defmodule Gateway.ClusterConfigReloaderTest do
   @moduledoc false
   # Integration test to check that the config cache is reloaded across cluster after a config change
-  use Gateway.AcceptanceCase
+  use Gateway.AcceptanceCase, async: true
+  require Logger
+
+  setup do
+    Gateway.DB.Schemas.API
+    |> Gateway.DB.Configs.Repo.delete_all()
+
+    on_exit(fn ->
+      Gateway.DB.Schemas.API
+      |> Gateway.DB.Configs.Repo.delete_all()
+    end)
+  end
 
   @tag pending: true
+  @tag cluster: true
   test "correct communication between processes" do
     # spawns two nodes:
     # node1@127.0.0.1 and node2@127.0.0.1
     Gateway.Cluster.spawn()
 
     api_id = create_api() |> get_body() |> get_in(["data", "id"])
-
-    on_exit(fn ->
-      Gateway.DB.Schemas.API
-      |> Gateway.DB.Configs.Repo.delete_all()
-    end)
 
     ensure_the_change_is_visible_on(:'node1@127.0.0.1')
     ensure_the_change_is_visible_on(:'node2@127.0.0.1')
@@ -31,16 +38,32 @@ defmodule Gateway.ClusterConfigReloaderTest do
     assert "New name" == check_api_on_node(api_id, :name, :'node2@127.0.0.1')
   end
 
-  defp ensure_the_change_is_visible_on(nodename) do
-    :rpc.block_call(nodename, Gateway.DB.Configs.Repo, :all, [Gateway.DB.Schemas.API])
+  defp ensure_the_change_is_visible_on(nodename, iteration \\ 1)
+  defp ensure_the_change_is_visible_on(_nodename, iteration) when iteration > 5,
+    do: flunk "Changes is not distributed to other nodes"
+  defp ensure_the_change_is_visible_on(nodename, iteration) do
+    case :rpc.block_call(nodename, Gateway.DB.Configs.Repo, :all, [Gateway.DB.Schemas.API]) do
+      [] ->
+        Logger.debug("Changes not visible, retry in 1 second..")
+        :timer.sleep(1000)
+        ensure_the_change_is_visible_on(nodename, iteration + 1)
+      apis ->
+        apis
+    end
   end
 
   defp check_api_on_node(api_id, field, nodename) do
-    nodename
+    node_cache = nodename
     |> :rpc.block_call(:ets, :lookup, [:config, {:api, api_id}])
-    |> hd()
-    |> elem(1)
-    |> Map.get(field)
+
+    case node_cache do
+      [] -> flunk "Changes is not distributed to node #{nodename}"
+      list ->
+        list
+        |> List.first()
+        |> elem(1)
+        |> Map.get(field)
+    end
   end
 
   defp update_api(api_id, field, value) do
