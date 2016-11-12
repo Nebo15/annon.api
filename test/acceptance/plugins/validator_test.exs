11b7862a108264ece02c29b281e9513f72fdc184
Andrew Dryga <andrew@dryga.com>
2016-11-12 23:00:23 +0200

    Refactored validator plugin tests

--- test/acceptance/plugins/validator_test.exs
+++ test/acceptance/plugins/validator_test.exs
@@ -3,37 +3,53 @@ defmodule Gateway.Acceptance.Plugin.ValidatorTest do
   use Gateway.AcceptanceCase
 
   @schema %{"type" => "object",
-            "properties" => %{
-              "foo" => %{ "type" => "number"},
-              "bar" => %{ "type" => "string"}
-            },
-            "required" => ["bar"]
-          }
+            "properties" => %{"foo" => %{"type" => "number"}, "bar" => %{ "type" => "string"}},
+            "required" => ["bar"]}
 
-  test "post hook with empty data" do
-    request_data = %{host: get_host(:public), path: "/test", port: get_port(:public), scheme: "http", method: ["POST"]}
+  setup do
+    api_path = "/my_validated_api"
+    api = :api
+    |> build_factory_params(%{
+      request: %{
+        method: ["GET", "POST", "PUT", "DELETE"],
+        scheme: "http",
+        host: get_endpoint_host(:public),
+        port: get_endpoint_port(:public),
+        path: api_path
+      }
+    })
+    |> create_api()
+    |> get_body()
 
-    data = :api
-    |> build_factory_params()
-    |> Map.put(:request, request_data)
-    |> Map.put(:plugins, [
-      %{name: "validator", is_enabled: true, settings: %{
-        "rules" => [
-          %{"methods" => ["GET", "POST"], "path" => ".*", "schema" => @schema}
-        ]
-      }}
-    ])
+    api_id = get_in(api, ["data", "id"])
 
-    "apis"
-    |> post(Poison.encode!(data), :management)
+    %{api_id: api_id, api_path: api_path}
+  end
+
+  test "post hook with empty data", %{api_id: api_id, api_path: api_path} do
+    validator_plugin = :validator_plugin
+    |> build_factory_params(%{settings: %{
+      schema: Poison.encode!(@schema)
+    }})
+
+    "apis/#{api_id}/plugins"
+    |> put_management_url()
+    |> post!(validator_plugin)
     |> assert_status(201)
 
-    "test"
-    |> get(:public)
-    |> assert_status(404)
+    Gateway.AutoClustering.do_reload_config
 
-    "test"
-    |> post("{}", :public)
+    assert %{
+      "error" => %{"type" => "validation_failed"}
+    } = api_path
+    |> put_public_url()
+    |> post!(%{data: "aaaa"})
     |> assert_status(422)
+    |> get_body()
+
+    api_path
+    |> put_public_url()
+    |> post!(%{bar: "foo"})
+    |> assert_status(404)
   end
 end
