e436efddd971ced6f3d51a767aafc97fc0586d21
Andrew Dryga <andrew@dryga.com>
2016-11-12 23:00:11 +0200

    Refactored ip restriction plugin

--- test/acceptance/plugins/ip_restriction_test.exs
+++ test/acceptance/plugins/ip_restriction_test.exs
@@ -2,48 +2,66 @@ defmodule Gateway.Acceptance.Plugin.IPRestrictionTest do
   @moduledoc false
   use Gateway.AcceptanceCase
 
-  @request %{
-    host: "localhost",
-    method: ["GET"],
-    path: "/random_path",
-    port: 5000,
-    scheme: "http"
-  }
+  setup do
+    api_path = "/my_ip_filtered_api"
+    api = :api
+    |> build_factory_params(%{
+      request: %{
+        method: ["GET", "POST", "PUT", "DELETE"],
+        scheme: "http",
+        host: get_endpoint_host(:public),
+        port: get_endpoint_port(:public),
+        path: api_path
+      }
+    })
+    |> create_api()
+    |> get_body()
 
-  test "check blacklist" do
+    api_id = get_in(api, ["data", "id"])
 
-    %{"ip_blacklist" => ["127.0.0.*"], "ip_whitelist" => ["128.30.50.245"]}
-    |> api_ip_restriction_data()
-    |> http_api_create()
+    %{api_id: api_id, api_path: api_path}
+  end
+
+  test "blacklists ipv4 addresses", %{api_id: api_id, api_path: api_path} do
+    ip_restriction_plugin = :ip_restriction_plugin
+    |> build_factory_params(%{settings: %{
+      ip_blacklist: ["127.0.0.*"],
+      ip_whitelist: ["128.30.50.245"]
+    }})
+
+    "apis/#{api_id}/plugins"
+    |> put_management_url()
+    |> post!(ip_restriction_plugin)
+    |> assert_status(201)
 
     Gateway.AutoClustering.do_reload_config
 
-    @request.path
-    |> String.replace_prefix("/", "")
-    |> get(:public)
-    |> assert_status(400)
+    assert %{
+      "error" => %{"message" => "You has been blocked from accessing this resource.", "type" => "forbidden"}
+    } = api_path
+    |> put_public_url()
+    |> get!()
+    |> assert_status(403)
+    |> get_body()
   end
 
-  test "check blacklist + whitelist" do
+  test "whitelist allows blacklistsed addresses", %{api_id: api_id, api_path: api_path} do
+    ip_restriction_plugin = :ip_restriction_plugin
+    |> build_factory_params(%{settings: %{
+      ip_blacklist: ["255.255.255.1", "127.0.0.*"],
+      ip_whitelist: ["192.168.0.1", "127.0.0.1"]
+    }})
 
-    %{"ip_blacklist" => ["255.255.255.1", "127.0.0.*"], "ip_whitelist" => ["192.168.0.1", "127.0.0.1"]}
-    |> api_ip_restriction_data()
-    |> http_api_create()
+    "apis/#{api_id}/plugins"
+    |> put_management_url()
+    |> post!(ip_restriction_plugin)
+    |> assert_status(201)
 
-    Gateway.AutoClustering.do_reload_config()
+    Gateway.AutoClustering.do_reload_config
 
-    @request.path
-    |> String.replace_prefix("/", "")
-    |> get(:public)
+    api_path
+    |> put_public_url()
+    |> get!()
     |> assert_status(404)
-  end
-
-  def api_ip_restriction_data(settings) when is_map(settings) do
-    :api
-    |> build_factory_params()
-    |> Map.put(:request, @request)
-    |> Map.put(:plugins, [
-      %{name: "ip_restriction", is_enabled: true, settings: settings},
-    ])
   end
 end
