feddd1a4870468fb1caff04712d47f9bc29a1710
Eugene Pirogov <iamexile@gmail.com>
2016-11-16 16:38:16 +0200

    Are you happy now, dogma?"

--- test/acceptance/smoke/file_uploading.ex
+++ test/acceptance/smoke/file_uploading.ex
@@ -39,11 +39,15 @@ defmodule Gateway.Acceptance.Smoke.FileUploadingTest do
   end
 
   test "A POST request from user reaches upstream" do
-    path = "https://httpbin.org/post"
     path = put_public_url("/httpbin")
 
+    parts = [
+      {:file, __ENV__.file}, {"some-name", "some-value"}
+    ]
+
     response =
-      HTTPoison.post!(path, {:multipart, [{:file, __ENV__.file}, {"some-name", "some-value"}]}, [{"X-Custom-Header", "custom-value"}])
+      path
+      |> HTTPoison.post!({:multipart, parts}, [{"X-Custom-Header", "custom-value"}])
       |> Map.get(:body)
       |> Poison.decode!
 
