739364c10cfa7fca9e37b798a7a54b1f4313dac8
Eugene Pirogov <iamexile@gmail.com>
2016-11-24 16:58:23 +0200

    Common logic should remain in plug for now

--- lib/gateway/cache_adapters/ets.ex
+++ lib/gateway/cache_adapters/ets.ex
@@ -1,40 +1,14 @@
 defmodule Gateway.CacheAdapters.ETS do
-  def find_api_by(conn) do
+  def find_api_by(scheme, host, port) do
     match_spec = %{
       request: %{
-        host: get_host(conn),
-        port: conn.port,
-        scheme: normalize_scheme(conn.scheme)
+        scheme: scheme,
+        host: host,
+        port: port
       }
     }
 
     :config
     |> :ets.match_object({:_, match_spec})
-    |> Enum.map(&elem(&1, 1))
-    |> find_matching_method(conn.method)
-    |> find_matching_path(conn.request_path)
-  end
-
-  defp get_host(conn) do
-    case Plug.Conn.get_req_header(conn, "x-host-override") do
-      [] -> conn.host
-      [override | _] -> override
-    end
-  end
-
-  defp normalize_scheme(scheme) when is_atom(scheme), do: Atom.to_string(scheme)
-  defp normalize_scheme(scheme), do: scheme
-
-  defp find_matching_method(apis, method) do
-    apis
-    |> Enum.filter(&Enum.member?(&1.request.methods, method))
-  end
-
-  defp find_matching_path(apis, path) do
-    apis
-    |> Enum.filter(&String.starts_with?(path, &1.request.path))
-    |> Enum.sort_by(&String.length(&1.request.path))
-    |> Enum.reverse
-    |> List.first
   end
 end
--- lib/gateway/plugins/api_loader.ex
+++ lib/gateway/plugins/api_loader.ex
@@ -9,7 +9,41 @@ defmodule Gateway.Plugins.APILoader do
   import Plug.Conn
 
   @doc false
-  def call(conn, _opts) do
-    put_private(conn, :api_config, Gateway.CacheAdapters.ETS.find_api_by(conn))
+  def call(conn, _opts), do: put_private(conn, :api_config, conn |> get_config)
+
+  def get_config(conn) do
+    scheme = normalize_scheme(conn.scheme)
+    host = get_host(conn)
+    port = conn.port
+
+    apis = Gateway.CacheAdapters.ETS.find_api_by(scheme, host, port)
+
+    apis
+    |> Enum.map(&elem(&1, 1))
+    |> find_matching_method(conn.method)
+    |> find_matching_path(conn.request_path)
+  end
+
+  defp get_host(conn) do
+    case get_req_header(conn, "x-host-override") do
+      [] -> conn.host
+      [override | _] -> override
+    end
+  end
+
+  defp normalize_scheme(scheme) when is_atom(scheme), do: Atom.to_string(scheme)
+  defp normalize_scheme(scheme), do: scheme
+
+  def find_matching_method(apis, method) do
+    apis
+    |> Enum.filter(&Enum.member?(&1.request.methods, method))
+  end
+
+  def find_matching_path(apis, path) do
+    apis
+    |> Enum.filter(&String.starts_with?(path, &1.request.path))
+    |> Enum.sort_by(&String.length(&1.request.path))
+    |> Enum.reverse
+    |> List.first
   end
 end
