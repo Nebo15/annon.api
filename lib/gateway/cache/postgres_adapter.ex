06de5729229cb567ebe187466603ab1159fd6d4e
Eugene Pirogov <iamexile@gmail.com>
2016-11-24 17:39:42 +0200

    Tiny refactoring

--- lib/gateway/cache/postgres_adapter.ex
+++ lib/gateway/cache/postgres_adapter.ex
@@ -3,13 +3,14 @@ defmodule Gateway.Cache.PostgresAdapter do
 
   alias Gateway.DB.Configs.Repo
   alias Gateway.DB.Schemas.API
+  alias Gateway.DB.Schemas.Plugin
 
   def find_api_by(scheme, host, port) do
     query = from a in API,
       where: fragment("request->'scheme' = ?", ^scheme),
       where: fragment("request->'host' = ?", ^host),
       where: fragment("request->'port' = ?", ^port),
-      join: Gateway.DB.Schemas.Plugin,
+      join: Plugin,
       preload: [:plugins]
 
     Repo.all(query)
