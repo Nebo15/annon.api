a67f6b33985195be26172432f8a4c79ee266071b
Eugene Pirogov <iamexile@gmail.com>
2016-11-24 17:24:41 +0200

    Postgres adapter

---
+++ lib/gateway/cache_adapters/postgres.ex
@@ -0,0 +1,16 @@
+defmodule Gateway.CacheAdapters.Postgres do
+  import Ecto.Query
+
+  alias Gateway.DB.Configs.Repo
+  alias Gateway.DB.Schemas.API
+
+  def find_api_by(scheme, host, port) do
+    query = from a in API,
+      where: fragment("request->'scheme' = ?", ^scheme),
+      where: fragment("request->'host' = ?", ^host),
+      where: fragment("request->'port' = ?", ^port),
+      preload: [:plugins]
+
+    Repo.all(query)
+  end
+end
