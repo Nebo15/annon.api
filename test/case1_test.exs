db4b0f9ebc4fb6563bd132553d59ece4e843d3fb
Eugene Pirogov <iamexile@gmail.com>
2016-11-25 12:30:46 +0200

    Add reproducible test cases
    Run via:
    
      mix test test/case*

---
+++ test/case1_test.exs
@@ -0,0 +1,25 @@
+defmodule Gateway.Acceptance.Controllers.APITest do
+  @moduledoc false
+  use Gateway.AcceptanceCase, async: true
+
+  describe "apis/:api_id/" do
+    test "PUT" do
+      body = create_api() |> get_body()
+
+      id = get_in(body, ["data", "id"])
+
+      "apis/#{id}"
+      |> put_management_url()
+      |> put!(%{name: "updated-name"})
+      |> assert_status(200)
+
+      body = "apis/#{id}"
+      |> put_management_url()
+      |> get!()
+      |> assert_status(200)
+      |> get_body()
+
+      assert "updated-name" == get_in(body, ["data", "name"])
+    end
+  end
+end
---
+++ test/case2_test.exs
@@ -0,0 +1,34 @@
+defmodule Gateway.Acceptance.Plugins.ACLTest do
+  @moduledoc false
+  use Gateway.AcceptanceCase, async: true
+
+  describe "JWT Strategy" do
+    test "Auth0 Flow is supported" do
+      api_path = "/my_jwt_authorized_api-" <> Ecto.UUID.generate() <> "/"
+      api_settings = %{
+        request: %{
+          methods: ["GET", "POST", "PUT", "DELETE"],
+          scheme: "http",
+          host: get_endpoint_host(:public),
+          port: get_endpoint_port(:public),
+          path: api_path
+        }
+      }
+      api = :api
+      |> build_factory_params(api_settings)
+      |> create_api()
+      |> get_body()
+
+      api_id = get_in(api, ["data", "id"])
+
+      jwt_plugin = build_factory_params(:jwt_plugin, %{settings: %{signature: build_jwt_signature("secret")}})
+
+      "apis/#{api_id}/plugins"
+      |> put_management_url()
+      |> post!(jwt_plugin)
+      |> assert_status(201)
+
+      :ok
+    end
+  end
+end
