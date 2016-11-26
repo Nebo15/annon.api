72219760535f1ac14dbac6df01ed915b098ed33a
Eugene Pirogov <iamexile@gmail.com>
2016-11-26 14:15:58 +0200

    More cleanup

--- lib/gateway/db/configs/repo.ex
+++ lib/gateway/db/configs/repo.ex
@@ -1,4 +1,11 @@
 defmodule Gateway.DB.Configs.Repo do
+  @moduledoc """
+  Main repository for DB that stores configuration.
+
+  This database doesn't need to have high performance, since all data is
+  [fetched once and cached in Annon](http://docs.annon.apiary.io/#introduction/general-features/caching-and-perfomance).
+  """
+
   use Ecto.Repo, otp_app: :gateway
   use Ecto.Pagging.Repo
 end
