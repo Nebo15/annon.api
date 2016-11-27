e06cc8522d7e8ec6c9f05c4b4937ab9d394ea809
Eugene Pirogov <iamexile@gmail.com>
2016-11-27 11:11:30 +0200

    Cleanup database

---
+++ priv/repos/gateway/migrations/20161127090151_cleanup_consumers.exs
@@ -0,0 +1,25 @@
+defmodule Gateway.DB.Configs.Repo.Migrations.CleanupConsumers do
+  use Ecto.Migration
+
+  def up do
+    drop table(:consumers)
+    drop table(:consumer_plugin_settings)
+  end
+
+  def down do
+    create table(:consumers, primary_key: false) do
+      add :external_id, :uuid
+      add :metadata, :map
+
+      timestamps()
+    end
+
+    create table(:consumer_plugin_settings) do
+      add :external_id, :uuid
+      add :plugin_id, :integer
+      add :settings, :map
+
+      timestamps()
+    end
+  end
+end
