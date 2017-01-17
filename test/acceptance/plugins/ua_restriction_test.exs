b3fb9066d878b301916f06dde5489255f2a7e566
MBatrak <maxim.batrak@finstar.com>
2017-01-17 18:58:07 +0200

    Added more tests

--- test/acceptance/plugins/ua_restriction_test.exs
+++ test/acceptance/plugins/ua_restriction_test.exs
@@ -2,6 +2,8 @@ defmodule Gateway.Acceptance.Plugins.UARestrictionTest do
   @moduledoc false
   use Gateway.AcceptanceCase, async: false
 
+  @user_agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
+
   setup do
     api_path = "/my_ua_filtered_api-" <> Ecto.UUID.generate() <> "/"
     api = :api
@@ -64,5 +66,46 @@ defmodule Gateway.Acceptance.Plugins.UARestrictionTest do
       |> get!()
       |> assert_status(404)
     end
+  end
+
+  test "blacklists chrome requests", %{api_id: api_id, api_path: api_path} do
+    ua_restriction_plugin = :ua_restriction_plugin
+    |> build_factory_params(%{settings: %{
+      blacklist: ["Chrome"]
+    }})
+
+    "apis/#{api_id}/plugins"
+    |> put_management_url()
+    |> post!(ua_restriction_plugin)
+    |> assert_status(201)
+
+    expected_result = %{"message" => "You have been blocked from accessing this resource.", "type" => "forbidden"}
+
+    actual_result = api_path
+    |> put_public_url()
+    |> get!([{"user-agent", @user_agent}])
+    |> assert_status(403)
+    |> get_body()
+    |> Map.get("error")
+
+    assert expected_result == actual_result
+  end
+
+  test "whitelist allows blacklisted user agents", %{api_id: api_id, api_path: api_path} do
+    ua_restriction_plugin = :ua_restriction_plugin
+    |> build_factory_params(%{settings: %{
+      blacklist: ["Linux"],
+      whitelist: ["Chrome"]
+    }})
+
+    "apis/#{api_id}/plugins"
+    |> put_management_url()
+    |> post!(ua_restriction_plugin)
+    |> assert_status(201)
+
+    api_path
+    |> put_public_url()
+    |> get!([{"user-agent", @user_agent}])
+    |> assert_status(404)
   end
 end
