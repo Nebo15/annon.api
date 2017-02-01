ef0cf6ef296c16065ac74f0cfc9a2d6783f898ab
Andrew Dryga <andrew@dryga.com>
2017-02-01 15:59:19 +0200

    Typo

--- bin/ci/init-statsd.sh
+++ bin/ci/init-statsd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh
+bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
