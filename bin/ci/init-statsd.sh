dc530634c30b36d5bc849ab0b62afe0ca58ddda5
Andrew Dryga <andrew@dryga.com>
2017-02-01 16:46:05 +0200

    dbg

--- bin/ci/init-statsd.sh
+++ bin/ci/init-statsd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-DD_API_KEY=14fff8b0c473a7e27bb5833c72f29b2f bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
-sudo sh -c "echo 'listen_port: 17123' > /etc/dd-agent/datadog.conf"
-sudo sh -c "echo 'use_dogstatsd: yes' > /etc/dd-agent/datadog.conf"
-sudo sh -c "echo 'dogstatsd_port: 8125' > /etc/dd-agent/datadog.conf"
+DD_API_KEY=14fff8b0c473a7e27bb5833c72f29b2f DD_INSTALL_ONLY=true bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
+sudo sh -c "echo 'listen_port: 17123' >> /etc/dd-agent/datadog.conf"
+sudo sh -c "echo 'use_dogstatsd: yes' >> /etc/dd-agent/datadog.conf"
+sudo sh -c "echo 'dogstatsd_port: 8125' >> /etc/dd-agent/datadog.conf"
 
 cat /etc/dd-agent/datadog.conf
 
+sudo /etc/init.d/datadog-agent stop
 sudo /etc/init.d/datadog-agent start
+
 exit 1
