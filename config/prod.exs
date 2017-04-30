9f7639e5c9f54784aa511ada08ba22277b60a3fa
Andrew Dryga <andrew@dryga.com>
2017-04-30 13:09:14 +0300

    Fixed typos in env configs

--- config/prod.exs
+++ config/prod.exs
@@ -25,5 +25,5 @@ config :ex_statsd,
 
 config :skycluster,
   strategy: {:system, :module, "SKYCLUSTER_STRATEGY", Cluster.Strategy.Kubernetes},
-  kubernetes_selector: {:system, "SKYCLUSTER_KUBERNATES_SELECTOR", "app=annon,component=api"},
-  kubernetes_node_basename: {:system, "SKYCLUSTER_KUBERNATES_NODE_BASENAME", "gateway"}
+  kubernetes_selector: {:system, "SKYCLUSTER_KUBERNETES_SELECTOR", "app=annon,component=api"},
+  kubernetes_node_basename: {:system, "SKYCLUSTER_KUBERNETES_NODE_BASENAME", "gateway"}
