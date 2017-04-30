ba1058a91131adf62182b39417e9c9cdee2de863
Andrew Dryga <andrew@dryga.com>
2017-04-30 14:51:17 +0300

    Apache doesn't distribute v3.9 anymore, use 3.10

--- bin/ci/init-db.sh
+++ bin/ci/init-db.sh
@@ -4,6 +4,6 @@ export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export MAX_HEAP_SIZE=128M
 export HEAP_NEWSIZE=24M
 java -version
-wget http://www.us.apache.org/dist/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz
+wget http://www.us.apache.org/dist/cassandra/3.10/apache-cassandra-3.10-bin.tar.gz
 tar -xzf apache-cassandra-3.9-bin.tar.gz
 sh ./apache-cassandra-3.9/bin/cassandra 2>&1 >/dev/null
