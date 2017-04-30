89ef17865dd746c155f454a243e181aab7a96429
Andrew Dryga <andrew@dryga.com>
2017-04-30 14:56:00 +0300

    Typos

--- bin/ci/init-db.sh
+++ bin/ci/init-db.sh
@@ -5,5 +5,5 @@ export MAX_HEAP_SIZE=128M
 export HEAP_NEWSIZE=24M
 java -version
 wget http://www.us.apache.org/dist/cassandra/3.10/apache-cassandra-3.10-bin.tar.gz
-tar -xzf apache-cassandra-3.9-bin.tar.gz
-sh ./apache-cassandra-3.9/bin/cassandra 2>&1 >/dev/null
+tar -xzf apache-cassandra-3.10-bin.tar.gz
+sh ./apache-cassandra-3.10/bin/cassandra 2>&1 >/dev/null
