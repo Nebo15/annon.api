82e80b5f7f62be837a82700d2b20ee384685e908
Andrew Dryga <andrew@dryga.com>
2017-05-01 18:19:24 +0300

    Dropped dead code

--- lib/annon_api/requests/request.ex
+++ lib/annon_api/requests/request.ex
@@ -1,6 +1,6 @@
 defmodule Annon.Requests.Request do
   @moduledoc """
-  Schema for saved requests and responses.
+  Schema for Requests Log.
   """
   use Ecto.Schema
 
@@ -44,58 +44,4 @@ defmodule Annon.Requests.Request do
 
     timestamps()
   end
-
-  # def changeset(struct, params \\ %{}) do
-  #   struct
-  #   |> cast(params, [:id, :idempotency_key, :ip_address, :status_code])
-  #   |> cast_embed(:request, with: &changeset_embeded_request/2)
-  #   |> cast_embed(:api, with: &changeset_embeded_api/2)
-  #   |> cast_embed(:request, with: &changeset_embeded_request/2)
-  #   |> cast_embed(:response, with: &changeset_embeded_response/2)
-  #   |> cast_embed(:latencies, with: &changeset_embeded_latencies/2)
-  #   |> validate_required([:response, :status_code])
-  # end
-
-  # def changeset_embeded_request(data, params \\ %{}) do
-  #   data
-  #   |> cast(params, [:method, :uri, :query, :headers, :body])
-  # end
-
-  # def changeset_embeded_response(data, params \\ %{}) do
-  #   data
-  #   |> cast(params, [:status_code, :headers, :body])
-  # end
-
-  # def changeset_embeded_latencies(data, params \\ %{}) do
-  #   data
-  #   |> cast(params, [:gateway, :upstream, :client_request])
-  # end
-
-  # def changeset_embeded_api(data, params \\ %{}) do
-  #   data
-  #   |> cast(params, [:name])
-  #   |> cast_embed(:request, with: &changeset_embeded_api_request/2)
-  # end
-
-  # def changeset_embeded_api_request(data, params \\ %{}) do
-  #   data
-  #   |> cast(params, [:scheme, :host, :port, :path])
-  # end
-
-  # def get_one_by(selector) do
-  #   Repo.one from Request,
-  #     where: ^selector,
-  #     limit: 1
-  # end
-
-  # def create_request(params) when is_map(params) do
-  #   %Request{}
-  #   |> changeset(params)
-  #   |> Repo.insert
-  # end
-
-  # def delete(request_id) do
-  #   Repo.delete_all from a in Request,
-  #     where: a.id == ^request_id
-  # end
 end
