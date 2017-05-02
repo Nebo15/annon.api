a060cb5275bd96e927f67dad57625a20defa8df1
Andrew Dryga <andrew@dryga.com>
2017-05-02 21:15:13 +0300

    Drop unused attributes

--- lib/annon_api/configuration/schemas/plugin.ex
+++ lib/annon_api/configuration/schemas/plugin.ex
@@ -3,20 +3,7 @@ defmodule Annon.Configuration.Schemas.Plugin do
   Schema for Plugins settings.
   """
   use Ecto.Schema
-  import Ecto.Changeset
-  import Annon.Validators.Settings
-  alias Annon.Configuration.Schemas.Plugin, as: PluginSchema
   alias Annon.Configuration.Schemas.API, as: APISchema
-
-  @type t :: %PluginSchema{
-    name: atom,
-    is_enabled: boolean,
-    settings: map
-  }
-
-  @valid_plugin_names [
-    "jwt", "validator", "acl", "proxy", "idempotency", "ip_restriction", "ua_restriction", "scopes", "cors"
-  ]
 
   @derive {Poison.Encoder, except: [:__meta__, :api]}
   schema "plugins" do
@@ -28,5 +15,4 @@ defmodule Annon.Configuration.Schemas.Plugin do
 
     timestamps()
   end
-
 end
