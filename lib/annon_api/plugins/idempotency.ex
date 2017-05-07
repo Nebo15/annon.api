8795636758b568ae81289a99568f88c4173e08fd
Andrew Dryga <andrew@dryga.com>
2017-05-07 21:09:25 +0300

    Refactored idempotency plugin

--- lib/annon_api/plugins/idempotency.ex
+++ lib/annon_api/plugins/idempotency.ex
@@ -16,35 +16,37 @@ defmodule Annon.Plugins.Idempotency do
   def settings_validation_schema,
     do: %{}
 
-  def execute(%Conn{method: method, body_params: params} = conn, _request, _settings)
+  def execute(%Conn{method: method, body_params: request_body} = conn, _request, _settings)
     when method in @idempotent_methods do
-    conn
-    |> Conn.get_req_header("x-idempotency-key")
-    |> load_log_request
-    |> validate_request(params)
-    |> normalize_resp(conn)
-  end
-  def execute(%Conn{} = conn, _request, _settings),
-    do: conn
+    with {:ok, idempotency_key} <- fetch_idempotency_key(conn),
+         {:ok, saved_request} <- Log.get_request_by(idempotency_key: idempotency_key),
+         true <- request_body_equal?(request_body, saved_request) do
 
-  defp load_log_request([key|_]) when is_binary(key) do
-    Log.get_request_by(idempotency_key: key)
-  end
-  defp load_log_request(_), do: nil
+      %RequestSchema{response: %{headers: headers, body: body}, status_code: status_code} = saved_request
 
-  defp validate_request({:ok, %RequestSchema{request: %{body: body}} = log_request}, params) do
-    {Map.equal?(params, body), log_request}
+      conn
+      |> Conn.merge_resp_headers(format_headers(headers))
+      |> Conn.send_resp(status_code, body)
+      |> Conn.halt()
+    else
+      :error -> conn
+      {:error, :not_found} -> conn
+      false -> render_duplicate_idempotency_key(conn)
+    end
   end
-  defp validate_request(_, _params), do: nil
 
-  defp normalize_resp({true, %RequestSchema{response: %{headers: headers, body: body}, status_code: status_code}}, conn)
-    do
-    conn
-    |> Conn.merge_resp_headers(format_headers(headers))
-    |> Conn.send_resp(status_code, body)
-    |> Conn.halt
+  defp fetch_idempotency_key(conn) do
+    case Conn.get_req_header(conn, "x-idempotency-key") do
+      [] -> :error
+      [idempotency_key|_] -> {:ok, idempotency_key}
+    end
   end
-  defp normalize_resp({false, _}, conn) do
+
+  defp request_body_equal?(body_params, %RequestSchema{request: %{body: body}}) do
+    Map.equal?(body_params, body)
+  end
+
+  defp render_duplicate_idempotency_key(conn) do
     "409.json"
     |> ErrorView.render(%{
       message: "You sent duplicate idempotency key but request params was different.",
@@ -58,8 +60,9 @@ defmodule Annon.Plugins.Idempotency do
     |> Response.send(conn, 409)
     |> Response.halt()
   end
-  defp normalize_resp(_, conn), do: conn
 
-  defp format_headers([]), do: []
-  defp format_headers([map|t]), do: [Enum.at(map, 0)] ++ format_headers(t)
+  defp format_headers([]),
+    do: []
+  defp format_headers([head | t]),
+    do: [Enum.at(head, 0)] ++ format_headers(t)
 end
