e55252b96fd7d5a53d73dca512e28cfff53b7812
Andrew Dryga <andrew@dryga.com>
2017-05-08 01:07:37 +0300

    Renamed ambiguous skycluster option

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ config :ex_statsd,
 config :skycluster,
   strategy: {:system, :module, "SKYCLUSTER_STRATEGY", Cluster.Strategy.Kubernetes},
   kubernetes_selector: {:system, "SKYCLUSTER_KUBERNETES_SELECTOR", "app=annon,component=api"},
-  kubernetes_node_basename: {:system, "SKYCLUSTER_KUBERNETES_NODE_BASENAME", "gateway"}
+  kubernetes_node_basename: {:system, "SKYCLUSTER_NODE_NAME", "annon_api"}
 
 # Do not print debug messages in production
 # and handle all other reports by Elixir Logger with JSON back-end
