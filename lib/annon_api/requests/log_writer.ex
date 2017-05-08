de47247cb41ab5f2c6bee23462dca5d216e8a197
Andrew Dryga <andrew@dryga.com>
2017-05-08 18:35:01 +0300

    Async log writer

---
+++ lib/annon_api/requests/log_writer.ex
@@ -0,0 +1,91 @@
+defmodule Annon.Requests.LogWriter do
+  @moduledoc """
+  This module provides interface to synchronously or asynchronously create Log Entries.
+  """
+  use GenServer
+  alias Annon.Requests.Log
+  alias Ecto.Changeset
+  require Logger
+
+  @doc """
+  Start a LogWriter worker.
+
+  `start_link` accepts optional `opts` as `Keyword`:
+    * `name` - name for a worker process;
+    * `subscriber` - name or pid of a process that wants to be notified when log is
+    written with `create_request_async/1`.
+  """
+  def start_link(opts \\ []) do
+    name = Keyword.get(opts, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: name)
+  end
+
+  @doc false
+  def init(opts) do
+    {:ok, opts}
+  end
+
+  @doc """
+  Creates a Request.
+
+  ## Examples
+
+      iex> create_request(%{field: value})
+      {:ok, %Annon.Requests.Request{}}
+
+      iex> create_request(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_request(attrs) do
+    Log.create_request(attrs)
+  end
+
+  @doc """
+  Asynchronously creates a Request.
+  If request insert operations is failed, error is logged to a console.
+
+  Accepts optional `opts` as `Keyword`:
+    * `name` - name or pid of a worker process.
+
+  ## Examples
+
+      iex> create_request(%{field: value})
+      :ok
+
+      iex> create_request(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_request_async(attrs, opts \\ []) do
+    name_or_pid = Keyword.get(opts, :name, __MODULE__)
+    case Log.change_request(attrs) do
+      %Changeset{valid?: false} = changeset ->
+        {:error, changeset}
+      changeset ->
+        GenServer.cast(name_or_pid, {:insert_request, changeset})
+        :ok
+    end
+  end
+
+  @doc false
+  def handle_cast({:insert_request, changeset}, opts) do
+    case Log.insert_request(changeset) do
+      {:ok, request} = message ->
+        maybe_notify_subscriber(message, opts)
+        {:noreply, opts}
+      {:error, changeset} = message ->
+        Logger.error("Failed to log request: #{inspect changeset}")
+        maybe_notify_subscriber(message, opts)
+        {:noreply, opts}
+    end
+  end
+
+  defp maybe_notify_subscriber(message, opts) do
+    case Keyword.fetch(opts, :subscriber) do
+      :error -> :ok
+      {:ok, subscriber} ->
+        send(subscriber, message)
+    end
+  end
+end
---
+++ test/unit/requests/log_writer_test.exs
@@ -0,0 +1,121 @@
+defmodule Annon.Requests.LogWriterTest do
+  use Annon.DataCase, async: true
+  alias Annon.Requests.LogWriter
+  alias Annon.Requests.Request
+
+  describe "create_request/1" do
+    test "with valid data creates a request" do
+      create_attrs = %{
+        id: "6f40ea08-00f9-4912-9472-4cd789facfa1",
+        idempotency_key: "cc9b19a8-4e6d-4237-9bb0-1137ab0d9f82",
+        ip_address: "129.168.1.10",
+        status_code: 200,
+        api: %{
+          id: "01",
+          name: "An API #01",
+          request: %{
+            host: "www.example01.com",
+            path: "/my_api/",
+            port: 80,
+            scheme: "http"
+          }
+        },
+        latencies: %{
+          client_request: 102,
+          gateway: 2,
+          upstream: 100
+        },
+        request: %{
+          body: %{},
+          headers: [%{"content-type" => "application/json"}],
+          method: "GET",
+          query: %{"key" => "value"},
+          uri: "/my_api/"
+        },
+        response: %{
+          body: "{}",
+          headers: [%{"content-type" => "application/json"}],
+          status_code: 200
+        }
+      }
+
+      assert {:ok, %Request{} = request} = LogWriter.create_request(create_attrs)
+
+      assert request.api.id == create_attrs.api.id
+      assert request.latencies.client_request == create_attrs.latencies.client_request
+      assert request.request.body == create_attrs.request.body
+      assert request.response.body == create_attrs.response.body
+      assert request.id == create_attrs.id
+      assert request.idempotency_key == create_attrs.idempotency_key
+      assert request.ip_address == create_attrs.ip_address
+      assert request.status_code == create_attrs.status_code
+    end
+
+    test "with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = LogWriter.create_request(%{})
+    end
+  end
+
+  describe "create_request_async/1" do
+    setup do
+      opts = [subscriber: self(), name: Annon.Requests.LogWriterTest]
+      {:ok, worker_pid} = Annon.Requests.LogWriter.start_link(opts)
+
+      Ecto.Adapters.SQL.Sandbox.allow(Annon.Requests.Repo, self(), worker_pid)
+
+      {:ok, %{writer_pid: worker_pid, worker_opts: opts}}
+    end
+
+    test "with valid data creates a request", %{worker_opts: opts} do
+      create_attrs = %{
+        id: "6f40ea08-00f9-4912-9472-4cd789facfa1",
+        idempotency_key: "cc9b19a8-4e6d-4237-9bb0-1137ab0d9f82",
+        ip_address: "129.168.1.10",
+        status_code: 200,
+        api: %{
+          id: "01",
+          name: "An API #01",
+          request: %{
+            host: "www.example01.com",
+            path: "/my_api/",
+            port: 80,
+            scheme: "http"
+          }
+        },
+        latencies: %{
+          client_request: 102,
+          gateway: 2,
+          upstream: 100
+        },
+        request: %{
+          body: %{},
+          headers: [%{"content-type" => "application/json"}],
+          method: "GET",
+          query: %{"key" => "value"},
+          uri: "/my_api/"
+        },
+        response: %{
+          body: "{}",
+          headers: [%{"content-type" => "application/json"}],
+          status_code: 200
+        }
+      }
+
+      assert :ok = LogWriter.create_request_async(create_attrs, opts)
+      assert_receive {:ok, %Request{} = request}
+
+      assert request.api.id == create_attrs.api.id
+      assert request.latencies.client_request == create_attrs.latencies.client_request
+      assert request.request.body == create_attrs.request.body
+      assert request.response.body == create_attrs.response.body
+      assert request.id == create_attrs.id
+      assert request.idempotency_key == create_attrs.idempotency_key
+      assert request.ip_address == create_attrs.ip_address
+      assert request.status_code == create_attrs.status_code
+    end
+
+    test "with invalid data returns error changeset", %{worker_opts: opts} do
+      assert {:error, %Ecto.Changeset{}} = LogWriter.create_request_async(%{}, opts)
+    end
+  end
+end
