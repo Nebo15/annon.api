87b8f54a8163b47a41a3d5555322509c008bd6c6
Andrew Dryga <andrew@dryga.com>
2017-05-16 19:16:46 +0300

    Track responses count and renamed metric names

--- lib/annon_api/monitoring/metrics_collector.ex
+++ lib/annon_api/monitoring/metrics_collector.ex
@@ -6,17 +6,18 @@ defmodule Annon.Monitoring.MetricsCollector do
   alias Annon.Monitoring.Latencies
 
   def track_request(_request_id, nil, opts),
-    do: increment("request_count", 1, opts)
+    do: increment("request.count", 1, opts)
   def track_request(_request_id, content_length, opts) do
-    increment("request_count", 1, opts)
-    histogram("request_size", content_length, opts)
+    increment("request.count", 1, opts)
+    histogram("request.size", content_length, opts)
   end
 
   def track_response(_request_id, latencies, opts) do
     %Latencies{client_request: client, upstream: upstream, gateway: gateway} = latencies
 
-    histogram("latencies_client", client, opts)
-    histogram("latencies_upstream", upstream, opts)
-    histogram("latencies_gateway", gateway, opts)
+    increment("responses.count", client, opts)
+    histogram("latencies.client", client, opts)
+    histogram("latencies.upstream", upstream, opts)
+    histogram("latencies.gateway", gateway, opts)
   end
 end
