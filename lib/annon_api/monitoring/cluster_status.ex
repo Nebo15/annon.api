e17b6b91131528dae801577faff5bace67eb8ad3
Andrew Dryga <andrew@dryga.com>
2017-05-24 17:47:18 +0300

    Typo

--- lib/annon_api/monitoring/cluster_status.ex
+++ lib/annon_api/monitoring/cluster_status.ex
@@ -10,8 +10,9 @@ defmodule Annon.Monitoring.ClusterStatus do
     nodes_status =
       Enum.reduce(Node.list(), [get_node_status()], fn remote_node, acc ->
         case :rpc.call(remote_node, Annon.Monitoring, :get_node_status, []) do
-          {:badrpc, reason} ->
-            Logger.error("Unable to fetch status of remote node #{inspect remote_node}, reason: #{inspect reason}")
+          {:badrpc, reason, rpc_err} ->
+            Logger.error("Unable to fetch status of remote node #{inspect remote_node}, reason: #{inspect reason}. " <>
+                         "RPC error: rpc_err")
             acc
           remote_node_status ->
             [remote_node_status] ++ acc
