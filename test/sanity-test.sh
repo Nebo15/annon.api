71e9dcca65ff98392c411af637944d874d0d0aab
Andrew Dryga <andrew@dryga.com>
2017-09-03 20:17:08 +0300

    Code style for sanity check

--- test/sanity-test.sh
+++ test/sanity-test.sh
@@ -3,20 +3,20 @@ set -xe
 
 # Create API
 API_ID=$(uuidgen)
-API=$(curl --silent --request PUT --header "Content-Type: application/json" http://localhost:4001/apis/$API_ID -d '{"api": {"request":{"scheme":"http","port":4000,"path":"/world","methods":["GET","POST"],"host":"localhost"},"name":"Sanity check"}}')
-API_NAME=$(echo $API | jq -r '.data.name')
+API=$(curl --silent --request PUT --header "Content-Type: application/json" http://localhost:4001/apis/${API_ID} -d '{"api": {"request":{"scheme":"http","port":4000,"path":"/world","methods":["GET","POST"],"host":"localhost"},"name":"Sanity check"}}')
+API_NAME=$(echo ${API} | jq -r '.data.name')
 
-if [ "$API_NAME" != "Sanity check" ]; then
-  echo "Unable to create API. Error response: $API"
+if [ "${API_NAME}" != "Sanity check" ]; then
+  echo "Unable to create API. Error response: ${API}"
   exit 1
 fi
 
 # Add proxy plugin
-PROXY=$(curl --silent --request PUT --header "Content-Type: application/json" http://localhost:4001/apis/$API_ID/plugins/proxy -d '{"plugin": {"name":"proxy","is_enabled":true,"settings":{"scheme":"http","port":80,"path":"/","host":"httpbin.org","strip_api_path":true}}}')
-PROXY_HOST=$(echo $PROXY | jq -r '.data.settings.host')
+PROXY=$(curl --silent --request PUT --header "Content-Type: application/json" http://localhost:4001/apis/${API_ID}/plugins/proxy -d '{"plugin": {"name":"proxy","is_enabled":true,"settings":{"scheme":"http","port":80,"path":"/","host":"httpbin.org","strip_api_path":true}}}')
+PROXY_HOST=$(echo ${PROXY} | jq -r '.data.settings.host')
 
-if [ "$PROXY_HOST" != "httpbin.org" ]; then
-  echo "Unable to create proxy plugin. Error response: $PROXY"
+if [ "${PROXY_HOST}" != "httpbin.org" ]; then
+  echo "Unable to create proxy plugin. Error response: ${PROXY}"
   exit 1
 fi
 
@@ -24,10 +24,10 @@ sleep 1
 
 # Issue a real request to proxy
 REQUEST=$(curl --silent --request GET http://localhost:4000/world/get?my_param=my_value)
-REQUEST_VALUE=$(echo $REQUEST | jq -r '.args.my_param')
+REQUEST_VALUE=$(echo ${REQUEST} | jq -r '.args.my_param')
 
-if [ "$REQUEST_VALUE" != "my_value" ]; then
-  echo "Unexpected responce from test upstream: $REQUEST"
+if [ "${REQUEST_VALUE}" != "my_value" ]; then
+  echo "Unexpected responce from test upstream: ${REQUEST}"
   exit 1
 else
   echo "Sanity check was successfull. All good!"
