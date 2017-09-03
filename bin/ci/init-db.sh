086f5c7148f13ae879aeb272278515a0a5a78de0
Andrew Dryga <andrew@dryga.com>
2017-09-03 19:47:30 +0300

    Bump PostgreSQL version for CI

--- bin/ci/init-db.sh
+++ bin/ci/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-POSTGRES_VERSION=9.5
+POSTGRES_VERSION=9.6
 
 echo "listen_addresses = '*'" >> /etc/postgresql/${POSTGRES_VERSION}/main/postgresql.conf
 echo "host all all 0.0.0.0/0 trust" >> /etc/postgresql/${POSTGRES_VERSION}/main/pg_hba.conf
